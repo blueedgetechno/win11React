1d59903f9d62ca2784bfa0f79d1bae8bcdf3965a
Malay Gain <malaygain10@gmail.com>
2021-08-30 23:18:13 +0530

    pseudo devtools and newtab added in edge

--- src/containers/applications/apps/edge.js
+++ src/containers/applications/apps/edge.js
@@ -5,9 +5,24 @@ import {Icon, Image, ToolBar} from '../../../utils/general';
 export const EdgeMenu = ()=>{
   const apps = useSelector(state => state.apps);
   const wnapp = useSelector(state => state.apps.edge);
-  const [url, setUrl] = useState("https://bing.com");
-  const [hist, setHist] = useState(["https://bing.com","https://bing.com"]);
+  const [url, setUrl] = useState(["https://bing.com"]);
+  const [hist, setHist] = useState([["https://bing.com","https://bing.com"]]);
   const dispatch = useDispatch();
+
+  const [devtool,setDevtool]=useState(false);
+  
+
+  const[console,setConsole]=useState(false);
+ 
+
+  const [tabs,setTabs]=useState([0]);
+
+  const [activeTab,setActiveTab]=useState(0);
+
+ const consoleUrl="https://jsconsole.com/";
+  
+ const element='<html lang="en" dir="ltr" crosspilot=""><head><meta name="theme-color" content="#4F4F4F"><meta name="description" content="Bing helps you turn information into action, making it faster and easier to go from searching to doing."><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">'
+ 
 
   const clickDispatch = (event)=>{
     var action = {
@@ -26,17 +41,45 @@ export const EdgeMenu = ()=>{
     var iframe = document.getElementById('isite');
     var x = e.target && e.target.dataset.payload;
 
-    if(iframe && x==0){
+    
+     
+    //iframe.src=url[activeTab];
+
+    if(iframe && x==0){                  //reload
       iframe.src = iframe.src;
-    }else if(iframe && x==1){
+    }else if(iframe && x==1){               //home
+      
+     // setActiveTab(id);
+
+     var tmp_hist=hist.map(row=>[...row]);
+      
+
+      tmp_hist[activeTab] = [url[activeTab],"https://www.bing.com"];  //++
+      setHist(tmp_hist);
+
+      var tmp=[...url];
+
+      tmp[activeTab]="https://www.bing.com";
+      setUrl(tmp);
+
+      /*
       setHist([url, "https://www.bing.com" ])
       setUrl("https://www.bing.com");
+
+      */
     }else if(iframe && x==2){
-      setHist([url, "https://www.google.com/webhp?igu=1"])
-      setUrl("https://www.google.com/webhp?igu=1");
-    }else if(iframe && x==3){
+                              //googling
+      //setActiveTab(id);    
+
+      hist[activeTab]= [url, "https://www.google.com/webhp?igu=1"] ;                      
+      setHist(hist);
+
+      url[activeTab]="https://www.google.com/webhp?igu=1";
+      setUrl(url);
+
+    }else if(iframe && x==3){                         //browsing
       if(e.key==="Enter"){
-        console.log("Wow");
+       // console.log("Wow");
         var qry = e.target.value;
 
         if(isValidURL(qry)){
@@ -50,14 +93,97 @@ export const EdgeMenu = ()=>{
 
         e.target.value = qry;
 
-        setHist([url, qry])
-        setUrl(qry);
+        
+
+        var tmp_hist=hist.map(row=>[...row]);
+      
+
+
+      tmp_hist[activeTab] = [url[activeTab], qry];  //++
+      setHist(tmp_hist);
+
+      var tmp=[...url];
+
+      tmp[activeTab]=qry;
+      setUrl(tmp);
+
+       
       }
-    }else if(x==4){
-      setUrl(hist[0])
-    }else if(x==5){
-      setUrl(hist[1])
+    }else if(x==4){             //left 
+      
+
+      var tmp=[...url];
+
+      tmp[activeTab]=hist[activeTab][0];
+      setUrl(tmp);
+    
+     
+
+    }else if(x==5){          //right
+      
+
+      var tmp=[...url];
+
+      tmp[activeTab]=hist[activeTab][1];
+      setUrl(tmp);
+      
     }
+    else if(x==6){
+
+
+                             // adding new tab
+
+      
+      
+      var temp=[...url];
+      temp.push("https://bing.com");
+      setUrl(temp);
+       
+      window.console.log(url[activeTab]);
+      
+      setActiveTab(tabs.length);
+     
+      
+      var tmp_hist=hist.map(row=>[...row]);
+      
+      tmp_hist.push(["https://bing.com","https://bing.com"]);
+      setHist(tmp_hist);
+      
+      tabs.push(tabs.length);
+      setTabs(tabs);
+     
+
+    }
+    else if(x==7)
+    {
+                              // removing a tab
+      
+           
+        url.splice(id,1);
+         
+        var tmp=[...url];
+        setUrl(tmp);
+        hist.splice(id,1);
+
+        var tmp_hist=[...hist]
+        setHist(tmp_hist);      
+
+       tabs.splice(id,1);
+      // tabs.pop();
+        var tmp_tab=[...tabs];
+       setTabs(tmp_tab);
+
+       
+    }
+    else if(x==8)       //switching tab
+    {
+      var id=e.target.key;
+      setActiveTab(id);
+      e.target.value=url[activeTab];
+
+    }
+
+
   }
 
   useEffect(()=>{
@@ -66,6 +192,13 @@ export const EdgeMenu = ()=>{
       dispatch({type: "EDGELINK"});
     }
   })
+
+
+  
+
+
+
+
 
   return (
     <div
@@ -78,11 +211,34 @@ export const EdgeMenu = ()=>{
         name="Microsoft Edge" float/>
       <div className="windowScreen flex flex-col">
         <div className="overTool flex">
-          <Icon src={wnapp.icon} width={14} margin="0 6px"/>
-          <div className="btab bg-gray-100">
-            <div>New Tab</div>
-            <Icon fafa="faTimes" click={wnapp.action} payload="close" width={10}/>
-          </div>
+         <Icon src={wnapp.icon} width={14} margin="0 6px"/>
+         <div className="btab bg-gray-100">
+                    <div>New Tab</div>
+                    <Icon fafa="faTimes" click={wnapp.action} payload="close" width={10}/>
+                    
+           </div>
+
+
+
+          {
+             
+             tabs.map((i)=>
+
+                 
+                   
+                  <div key={i} >
+                    <div className="btab bg-gray-100" onClick={action} payload={8}>
+                      <div>New Tab</div>
+                      <Icon fafa="faTimes" onClick={action} payload={7} width={10}/>
+                    
+                    </div>
+                  </div>
+            
+                 
+             )
+          }
+
+          <Icon fafa="faPlus" onClick={action} payload={6} width={10} margin="0 5"/>
         </div>
         <div className="restWindow flex-grow flex flex-col">
           <div
@@ -96,20 +252,67 @@ export const EdgeMenu = ()=>{
                 className="ltShad w-full bg-gray-0 h-6 px-4 text-gray-900"
                 onKeyDown={action}
                 data-payload={3}
-                defaultValue={url}
+                defaultValue={url[activeTab]}
                 placeholder="Type url or a query to search"
                 type="text"/>
                 <Icon className="z-1 handcr"
                   src="google" ui onClick={action}
                   payload={2} width={14} margin="0 10px"/>
+
+              <Icon className="z-1 handcr" fafa="faCode" onClick={()=>{setDevtool(!devtool)}}  width={14} margin="0 5"/>
             </div>
           </div>
-          <div className="siteFrame flex-grow overflow-hidden">
-            {wnapp.hide?null:(
-              <iframe src={url} id="isite" className="w-full h-full"
+          <div className="siteFrame flex flex-grow overflow-hidden">
+            {wnapp.hide || devtool?null:(
+                <iframe src={url[activeTab]} id="isite" className="w-full h-full"
+                  frameborder="0">
+                </iframe>
+                
+              )}
+
+
+
+             {devtool?(
+                
+                <iframe src={url[activeTab]} id="isite" className="w-4/5 h-full"
                 frameborder="0">
-              </iframe>
-            )}
+                </iframe> 
+                  
+                )
+
+              :null}
+             
+             {devtool?(<div className="w-1/5"> 
+
+               <div class="grid grid-cols-2 gap-4">
+                   <button class="w-15 h-10 items-center   border-gray-300 " onClick={()=>setConsole(!console)}>Element</button>
+                   <button class="w-15 h-10  items-center   border border-gray-300"  onClick={()=>setConsole(!console)} >Console</button>
+               </div>
+
+               
+
+               {!console?(
+                 <div id='element' className="w-full h-full ">
+                         {element}
+                 </div>
+               ):null
+
+               }
+               {console?(
+                 
+                   <iframe src={consoleUrl} className="w-full h-full"></iframe>
+                 
+               ):null
+
+               }
+          
+             </div>):null}
+             
+
+
+
+            
+            
           </div>
         </div>
       </div>
