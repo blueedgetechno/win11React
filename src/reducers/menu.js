2e46a90b4de01715189a20ea628ca9ffd7743f38
Yashash <yashash.pugalia2004@gmail.com>
2021-10-26 12:57:54 +0530

    improved right click menu
    added few icons, adjusted margin&padding to make it look a bit more like win11

--- src/reducers/menu.js
+++ src/reducers/menu.js
@@ -9,167 +9,208 @@ const defState = {
     desk: [
       {
         name: "View",
-        opts: [{
-          name: "Large icons",
-          action: "changeIconSize",
-          payload: "large"
-        }, {
-          name: "Medium icons",
-          action: "changeIconSize",
-          payload: "medium"
-        }, {
-          name: "Small icons",
-          action: "changeIconSize",
-          payload: "small",
-          dot: true
-        }, {
-          type: "hr"
-        }, {
-          name: "Show desktop icons",
-          action: "deskHide",
-          check: true
-        }]
-      }, {
+        icon: "ui/view",
+        opts: [
+          {
+            name: "Large icons",
+            action: "changeIconSize",
+            payload: "large",
+          },
+          {
+            name: "Medium icons",
+            action: "changeIconSize",
+            payload: "medium",
+          },
+          {
+            name: "Small icons",
+            action: "changeIconSize",
+            payload: "small",
+            dot: true,
+          },
+          {
+            type: "hr",
+          },
+          {
+            name: "Show desktop icons",
+            action: "deskHide",
+            check: true,
+          },
+        ],
+      },
+      {
         name: "Sort by",
-        opts: [{
-          name: "Name",
-          action: "changeSort",
-          payload: "name"
-        }, {
-          name: "Size",
-          action: "changeSort",
-          payload: "size"
-        }, {
-          name: "Date modified",
-          action: "changeSort",
-          payload: "date"
-        }]
-      }, {
+        icon: "ui/sort",
+        opts: [
+          {
+            name: "Name",
+            action: "changeSort",
+            payload: "name",
+          },
+          {
+            name: "Size",
+            action: "changeSort",
+            payload: "size",
+          },
+          {
+            name: "Date modified",
+            action: "changeSort",
+            payload: "date",
+          },
+        ],
+      },
+      {
         name: "Refresh",
-        action: "refresh"
-      }, {
-        type: "hr"
-      }, {
-        name: "Paste",
-        dsb: true
-      }, {
-        name: "Paste Shortcut",
-        dsb: true
-      },{
-        name: "Undo Delete"
-      },{
+        action: "refresh",
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "Next desktop background",
-        action: "WALLNEXT"
-      }, {
+        action: "WALLNEXT",
+      },
+      {
         name: "Open in Terminal",
-        icon: 'terminal',
+        icon: "terminal",
         action: "OPENTERM",
-        payload: "C:\\Users\\Blue\\Desktop"
-      }, {
-        type: "hr"
-      }, {
+        payload: "C:\\Users\\Blue\\Desktop",
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "New",
-        dsb: true
-      }, {
-        type: "hr"
-      }, {
+        icon: "ui/new",
+        dsb: true,
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "Personalize",
-        icon: 'win/themes',
-        dsb: true
-      },{
+        icon: "win/themes",
+        dsb: true,
+      },
+      {
         name: "About",
         action: "DESKABOUT",
-        icon: 'win/info',
-        payload: true
-      }
+        icon: "win/info",
+        payload: true,
+      },
     ],
-    task: [{
+    task: [
+      {
         name: "Align icons",
-        opts: [{
-          name: "Left",
-          action: "changeTaskAlign",
-          payload: "left"
-        }, {
-          name: "Center",
-          action: "changeTaskAlign",
-          payload: "center",
-          dot: true
-        }]
-      },{
-        type: "hr"
-      }, {
+        opts: [
+          {
+            name: "Left",
+            action: "changeTaskAlign",
+            payload: "left",
+          },
+          {
+            name: "Center",
+            action: "changeTaskAlign",
+            payload: "center",
+            dot: true,
+          },
+        ],
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "Search",
-        opts: [{
-          name: "Show",
-          action: "TASKSRCH",
-          payload: true
-        }, {
-          name: "Hide",
-          action: "TASKSRCH",
-          payload: false
-        }]
-      }, {
+        opts: [
+          {
+            name: "Show",
+            action: "TASKSRCH",
+            payload: true,
+          },
+          {
+            name: "Hide",
+            action: "TASKSRCH",
+            payload: false,
+          },
+        ],
+      },
+      {
         name: "Widgets",
-        opts: [{
-          name: "Show",
-          action: "TASKWIDG",
-          payload: true
-        }, {
-          name: "Hide",
-          action: "TASKWIDG",
-          payload: false
-        }]
-      },{
-        type: "hr"
-      }, {
+        opts: [
+          {
+            name: "Show",
+            action: "TASKWIDG",
+            payload: true,
+          },
+          {
+            name: "Hide",
+            action: "TASKWIDG",
+            payload: false,
+          },
+        ],
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "Show Desktop",
-        action: "SHOWDSK"
-      }
+        action: "SHOWDSK",
+      },
     ],
-    app: [{
+    app: [
+      {
         name: "Open",
         action: "performApp",
-        payload: "open"
-      },{
-        type: "hr"
-      },{
+        payload: "open",
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "Open file location",
-        dsb: true
-      },{
+        dsb: true,
+      },
+      {
         name: "Run as Administration",
         action: "performApp",
         payload: "open",
-        icon: "win/shield"
-      },{
-        type: "hr"
-      },{
+        icon: "win/shield",
+      },
+      {
+        type: "hr",
+      },
+      {
         name: "Unpin from start",
-        dsb: true
-      },{
+        dsb: true,
+      },
+      {
         name: "Pin to taskbar",
-        dsb: true
-      },{
+        dsb: true,
+      },
+      {
         name: "Delete shortcut",
         action: "performApp",
-        payload: "delshort"
-      },{
+        payload: "delshort",
+      },
+      {
         name: "Delete",
         action: "delApp",
-        payload: "delete"
-      },{
+        payload: "delete",
+      },
+      {
         name: "Rename",
-        dsb: true
-      },{
+        dsb: true,
+      },
+      {
         name: "Properties",
-        dsb: true
-      }
-    ]
-  }
+        dsb: true,
+      },
+    ],
+  },
 };
 
 const menusReducer = (state = defState, action) => {
   var tmpState = {
-    ...state
+    ...state,
   };
   if (action.type == "MENUHIDE") {
     tmpState.hide = true;
@@ -178,15 +219,15 @@ const menusReducer = (state = defState, action) => {
     tmpState.top = (action.payload && action.payload.top) || 272;
     tmpState.left = (action.payload && action.payload.left) || 430;
     tmpState.opts = (action.payload && action.payload.menu) || "desk";
-    tmpState.attr = action.payload && action.payload.attr
-    tmpState.dataset = action.payload && action.payload.dataset
+    tmpState.attr = action.payload && action.payload.attr;
+    tmpState.dataset = action.payload && action.payload.dataset;
   } else if (action.type == "MENUCHNG") {
     tmpState = {
-      ...action.payload
+      ...action.payload,
     };
   }
 
   return tmpState;
-}
+};
 
 export default menusReducer;
