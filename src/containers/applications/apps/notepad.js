4bb8a4969e3c2d55991d1f14a96d50289a64bed1
Blue edge <blueedgetechno@gmail.com>
2021-11-12 16:14:27 +0530

    fetch theme acc. to user pref, spotify font fix, about panel color fix

--- src/containers/applications/apps/notepad.js
+++ src/containers/applications/apps/notepad.js
@@ -15,7 +15,7 @@ export const Notepad = ()=>{
        zIndex: wnapp.z
        }} data-hide={wnapp.hide} id={wnapp.icon+"App"}>
       <ToolBar app={wnapp.action} icon={wnapp.icon}
-        name="Untitled - Notepad"/>
+        name="Untitled - Notepad" noinvert/>
       <div className="windowScreen flex flex-col" data-dock="true">
         <div className="flex text-xss pb-1 border-gray-200 border-0 border-b-2 border-solid">
           <div className="mx-2">File</div>
