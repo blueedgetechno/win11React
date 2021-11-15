d5cad96f9c7734133a14080e5233a21e45442353
Blue edge <blueedgetechno@gmail.com>
2021-11-16 02:00:23 +0530

    drag windows around

--- src/containers/applications/draft.js
+++ src/containers/applications/draft.js
@@ -15,7 +15,7 @@ export const IFrame = (props)=>{
         ...(wnapp.size=="cstm"?wnapp.dim:null),
         zIndex: wnapp.z
       }} data-hide={wnapp.hide} id={wnapp.icon+"App"}>
-      <ToolBar app={wnapp.action} icon={wnapp.icon}
+      <ToolBar app={wnapp.action} icon={wnapp.icon} noinvert
         name={wnapp.name} invert={data.invert==true?true:null}/>
       <div className="windowScreen flex flex-col" data-dock="true">
         <div className="restWindow flex-grow flex flex-col">
