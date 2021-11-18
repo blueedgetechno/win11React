4e8caf5f85f792238f206d4a2b4af802bc8e713a
andrewstech <info@andrewstech.me>
2021-11-18 13:48:17 +0000

    Add worker

---
+++ public/worker.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'pwa-task-manager';
+var urlsToCache = [
+  '/',
+  '/404',
+  '/completed'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
