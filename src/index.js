3ec1b5958587be2f887b929148de28fee15b6a3f
andrewstech <info@andrewstech.me>
2021-11-18 13:53:23 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import store from './reducers';
 import {Provider} from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+
 
 // store.subscribe(()=> console.log(store.getState()));
 // store.dispatch({ type: 'INCREMENT'});
+serviceWorker.register();
 
 ReactDOM.render(
   <Provider store={store}>
