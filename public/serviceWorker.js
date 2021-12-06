7754cd4870066fad7be3f3d0cca3384ff00a8192
andrewstech <info@andrewstech.me>
2021-12-06 16:08:41 +0000

    Create serviceWorker.js

---
+++ public/serviceWorker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html'];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('Opened cache');
+
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then(() => {
+			return fetch(event.request).catch(() => caches.match('index.html'));
+		})
+	);
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+	const cacheWhitelist = [];
+	cacheWhitelist.push(CACHE_NAME);
+
+	event.waitUntil(
+		caches.keys().then((cacheNames) =>
+			Promise.all(
+				cacheNames.map((cacheName) => {
+					if (!cacheWhitelist.includes(cacheName)) {
+						return caches.delete(cacheName);
+					}
+				})
+			)
+		)
+	);
+});
