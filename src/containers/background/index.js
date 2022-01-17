e17e5a2ea46a7a7e9b7216ae7cca87f435acf0b6
Yashash <yashash1511@gmail.com>
2022-01-17 09:50:01 +0530

    updated lockscreen loader
    using fluent-ui progress-ring

--- src/containers/background/index.js
+++ src/containers/background/index.js
@@ -52,8 +52,8 @@ export const BootScreen = (props) => {
       <div className={blackout ? "hidden" : ""}>
         <Image src="asset/bootlogo" w={180} />
         <div className="mt-48" id="loader">
-          <svg class="svg-container" height="48" width="48" viewBox="0 0 100 100">
-            <circle class="loader-svg" cx="50" cy="50" r="45"></circle>
+          <svg class="progressRing" height={48} width={48} viewBox="0 0 16 16">
+            <circle cx="8px" cy="8px" r="7px"></circle>
           </svg>
         </div>
       </div>
