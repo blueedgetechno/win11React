993aff836cf12e04ca03e0db280607ab159ec7b2
Lukas Batema <lukasbatema@gmail.com>
2022-02-01 07:20:40 -0500

    fix another typo…

--- src/containers/applications/apps/terminal.js
+++ src/containers/applications/apps/terminal.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
-import BrythonRunner from 'bythron-runner'
+import BrythonRunner from 'brython-runner';
 
 import {Icon, Image, ToolBar} from '../../../utils/general';
 import dirs from './assets/dir.json';
@@ -22,8 +22,6 @@ const useScript = url => {
 };
 
 export default useScript;
-
-
 
 const runner = new BrythonRunner({
     stdout: {
