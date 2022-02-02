bee9af404c2882dc910dcecb077be77498cc1243
Blue edge <54899800+blueedgetechno@users.noreply.github.com>
2022-02-02 12:59:29 +0530

    BrythonRunner Introduced, thanks to sheer will of @andrewstech. "python" command in windows terminal (#289)
    BrythonRunner problem solved
    
    Authored-by: andrewstech <info@andrewstech.me>
    Co-authored-by: Lukas Batema <lukasbatema@gmail.com>

--- public/console.js
+++ public/console.js
@@ -3,3 +3,28 @@ console.log('%cWin11React is a project by blueedgetechno and ascended by andrews
 function removeWarning() {
   document.body.dataset.force = true;
 }
+
+window.pythonRunner = new BrythonRunner({
+  stdout: {
+    write(content) {
+      window.pythonResult = content;
+      window.pythonErr = 0;
+    },
+    flush() {},
+  },
+  stderr: {
+    write(content) {
+      window.pythonResult = content;
+      window.pythonErr = 1;
+    },
+    flush() {},
+  },
+  stdin: {
+    async readline() {
+      var userInput = prompt();
+      console.log('Received StdIn: ' + userInput);
+      return userInput;
+    },
+  },
+  onInit() {}
+});
