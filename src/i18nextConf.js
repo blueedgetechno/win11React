3ae6bcaa582b6c8005cddf18547c316cb3cd790c
andrewstech <info@andrewstech.me>
2022-03-25 23:24:24 +0000

    Update i18nextConf.js

--- src/i18nextConf.js
+++ src/i18nextConf.js
@@ -2,6 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { i18nextPlugin } from 'translation-check';
+import { showTranslations } from 'translation-check';
 
 const fallbackLng = ['en'];
 const availableLanguages = ['en', 'fr'];
@@ -20,6 +22,8 @@ i18n
     debug: false,
 
     whitelist: availableLanguages,
+    
+    showTranslations(i18next),
 
     interpolation: {
       escapeValue: false, // no need for react. it escapes by default
