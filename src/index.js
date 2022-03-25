6b77b43e3397eb664dba8c97b791522320ac7bed
andrewstech <info@andrewstech.me>
2022-03-25 22:35:38 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import ‘./i18nextConf’;
 import App from './App';
 import store from './reducers';
 import {Provider} from 'react-redux';
