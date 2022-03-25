26fd96af7cbd86d5b5de8f9c4d2964e0e8bf37d8
andrewstech <info@andrewstech.me>
2022-03-25 22:29:45 +0000

    Create i18 config

---
+++ src/i18nextConf.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const fallbackLng = ['en'];
+const availableLanguages = ['en', 'fr'];
+
+i18n
+  .use(Backend) // load translations using http (default public/assets/locals/en/translations)
+  .use(LanguageDetector) // detect user language
+  .use(initReactI18next) // pass the i18n instance to react-i18next.
+  .init({
+    fallbackLng, // fallback language is english.
+
+    detection: {
+      checkWhitelist: true, // options for language detection
+    },
+
+    debug: false,
+
+    whitelist: availableLanguages,
+
+    interpolation: {
+      escapeValue: false, // no need for react. it escapes by default
+    },
+  });
+
+export default i18n;
