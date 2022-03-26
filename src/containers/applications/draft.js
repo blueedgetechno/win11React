85a2c2bd5a7139a74ed958360d942fed72c8036b
andrewstech <info@andrewstech.me>
2022-03-26 22:55:00 +0000

    Add camera support to iframe

--- src/containers/applications/draft.js
+++ src/containers/applications/draft.js
@@ -28,3 +28,30 @@ export const IFrame = (props)=>{
     </div>
   )
 }
+
+export const IFramePLUS = (props)=>{
+  const apps = useSelector(state => state.apps);
+  const wnapp = useSelector(state => state.apps[props.icon]);
+  const dispatch = useDispatch();
+  var data = wnapp.data;
+
+  return wnapp.hide?null:(
+    <div data-size={wnapp.size} className={"floatTab dpShad " +
+      (data.invert!=true?"lightWindow":"darkWindow")
+    } data-max={wnapp.max} style={{
+        ...(wnapp.size=="cstm"?wnapp.dim:null),
+        zIndex: wnapp.z
+      }} data-hide={wnapp.hide} id={wnapp.icon+"App"}>
+      <ToolBar app={wnapp.action} icon={wnapp.icon} size={wnapp.size}
+        name={wnapp.name} invert={data.invert==true?true:null} noinvert/>
+      <div className="windowScreen flex flex-col" data-dock="true">
+        <div className="restWindow flex-grow flex flex-col">
+          <div className="flex-grow overflow-hidden">
+            <iframe src={data.url} allow="camera;microphone" className="w-full h-full"
+              frameborder="0"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
