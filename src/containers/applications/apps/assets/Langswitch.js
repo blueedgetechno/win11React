5b8555c51c6577a2e64a64247447d07d5b94aa97
andrewstech <info@andrewstech.me>
2022-04-09 18:37:41 +0100

    Add lang to switch

--- src/containers/applications/apps/assets/Langswitch.js
+++ src/containers/applications/apps/assets/Langswitch.js
@@ -11,8 +11,19 @@ function LangSwitch() {
           i18next.changeLanguage(e.target.value)
         }
       >
+        <option value="da">Danish</option>
+        <option value="de">German</option>
         <option value="en">English</option>
+        <option value="es">Spanish</option>
         <option value="fr">French</option>
+        <option value="hi">Hindi</option>
+        <option value="hu">Hungarian</option>
+        <option value="ja">Japanese</option>
+        <option value="ko">Korean</option>
+        <option value="nl">Dutch</option>
+        <option value="ru">Russian</option>
+        <option value="tr">Turkish</option>
+        <option value="zh">Chinese</option>
       </select>
     </div>
   );
