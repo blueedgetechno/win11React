fa15b365a8ec3f3dcc6f9c6cae554ef2eec69256
andrewstech <info@andrewstech.me>
2022-04-09 18:21:50 +0100

    Fix lang codes

--- src/i18nextConf.js
+++ src/i18nextConf.js
@@ -5,7 +5,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { i18nextPlugin } from 'translation-check';
 
 const fallbackLng = ['en'];
-const availableLanguages = ['en', 'da-DK', 'de-DE', 'en-GB', 'es-ES', 'fr-FR', 'hi-IN', 'hu-HU', 'ko-KR', 'ru-RU', 'tr-TR', 'zh-CN', 'nl-NL', 'ja-JP'];
+const availableLanguages = ['en', 'da', 'de', 'es', 'fr', 'hi', 'hu', 'ja', 'ko', 'nl', 'ru', 'tr', 'zh'];
 
 i18n
   .use(Backend) // load translations using http (default public/assets/locals/en/translations)
