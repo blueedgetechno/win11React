c008708f65016dbd56b8de8636e890b6c3bd94da
Yashash <yashash1511@gmail.com>
2022-05-08 02:26:09 +0530

    taskbar navmenu

--- src/containers/applications/apps/taskmanager.js
+++ src/containers/applications/apps/taskmanager.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Icon, Image, ToolBar } from "../../../utils/general";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ToolBar } from "../../../utils/general";
 import "./assets/taskmanager.scss";
 
 export const Taskmanager = () => {
   const apps = useSelector((state) => state.apps);
   const wnapp = useSelector((state) => state.apps.taskmanager);
   const dispatch = useDispatch();
+
+  const [page, setPage] = useState("Processes");
+  const [nav, setNav] = useState("");
+
+  const pages = [
+    "Processes",
+    "Performance",
+    "App history",
+    "Startup apps",
+    "Users",
+    "Details",
+    "Services",
+    "Settings",
+  ];
 
   return (
     <div
@@ -28,8 +42,34 @@ export const Taskmanager = () => {
       />
       <div className="windowScreen flex flex-col" data-dock="true">
         <div className="restWindow flex-grow flex flex-col">
-          <div className="flex-grow grid place-items-center text-3xl">
-            Coming soon? maybe
+          <nav className={nav}>
+            {pages.map((e) => {
+              return (
+                <div
+                  key={e}
+                  className={`navLink ${e === page ? "selected" : ""}`}
+                  onClick={() => {
+                    setPage(e);
+                  }}
+                >
+                  <span className="someIcon">-</span>
+                  <span className="pageName">{e}</span>
+                </div>
+              );
+            })}
+            <div className="marker"></div>
+          </nav>
+          <main>some nonsense graphs here </main>
+          <div className="navMenuBtn" onClick={() => setNav(nav ? "" : "open")}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 48 48"
+              width={24}
+              height={24}
+            >
+              <path d="M5.5 9a1.5 1.5 0 1 0 0 3h37a1.5 1.5 0 1 0 0-3h-37zm0 13.5a1.5 1.5 0 1 0 0 3h37a1.5 1.5 0 1 0 0-3h-37zm0 13.5a1.5 1.5 0 1 0 0 3h37a1.5 1.5 0 1 0 0-3h-37z" />
+            </svg>
           </div>
         </div>
       </div>
