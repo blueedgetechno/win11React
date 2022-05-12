0ea2825a99867314585fa1aa85ebccf1da3dee15
andrewstech <info@andrewstech.me>
2022-05-12 17:56:54 +0100

    FIX

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 import store from "./reducers";
 import { Provider } from "react-redux";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 Sentry.init({
   dsn: "https://6c16d34365334e0fbee992044f9d223b@o575799.ingest.sentry.io/6251530",
