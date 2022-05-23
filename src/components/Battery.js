fecd1472bf1e81e4a903b933550e3ce0200e36ea
Yashash <yashash1511@gmail.com>
2022-05-23 18:45:48 +0530

    bt

--- src/components/Battery.js
+++ src/components/Battery.js
@@ -1,20 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Icon } from "../utils/general";
 
 const Battery = ({ pct }) => {
-  const batteryref = useRef(null);
   // var divtitle = "Battery status: " + level + "% " + (charging ? "available (plugged in)" : "remaining");
 
-  const [batterylevel, setbatterylevel] = useState(100);
+  const [btLevel, setbtLevel] = useState(100);
 
   const changebatterystatus = (bt) => {
     let level = bt.level * 100 || 100;
-
     if (bt.charging) {
-      setbatterylevel(-level);
-    } else {
-      setbatterylevel(level);
+      level = -level;
     }
+    setbtLevel(level);
   };
 
   useEffect(() => {
@@ -38,26 +35,20 @@ const Battery = ({ pct }) => {
     return () => {};
   }, []);
 
-  let charging = batterylevel < 0;
-  let level = Math.round(Math.abs(batterylevel));
-
-  useEffect(() => {
-    batteryref.current.style.width = `${level}%`;
-    return () => {};
-  }, [level, charging]);
+  let btPct = Math.round(Math.abs(btLevel)) + "%";
 
   return (
     <>
       <div className="uicon taskIcon">
         <span className="battery">
-          {charging ? (
+          {btLevel < 0 ? (
             <Icon className="btPlug" fafa="faBoltLightning" width={8} />
           ) : null}
           <i className="fa fa-battery-empty"></i>
-          <i className="fa fa-battery-4 animate" ref={batteryref}></i>
+          <i className="fa fa-battery-4 animate" style={{ width: btPct }}></i>
         </span>
       </div>
-      {pct ? <div className="text-xs">{level}%</div> : ""}
+      {pct ? <div className="text-xs">{Math.round(Math.abs(btLevel))}%</div> : ''}
     </>
   );
 };
