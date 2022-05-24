7d16a2068d90ec534131c4058bf0e7c2c42fbed7
Yashash Pugalia <yashash1511@gmail.com>
2022-05-24 15:29:06 +0530

    OOBE (#469)
    Co-Authored-By: Cup Of Coffe <90518786+cupofcoffebruh@users.noreply.github.com>

--- src/containers/applications/apps/getstarted.js
+++ src/containers/applications/apps/getstarted.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ToolBar } from "../../../utils/general";
+import { Icon, ToolBar } from "../../../utils/general";
+import countries from "./assets/countrylist.json";
 import "./assets/getstarted.scss";
+import { useState } from "react";
 
 export const Getstarted = () => {
   const apps = useSelector((state) => state.apps);
   const wnapp = useSelector((state) => state.apps.getstarted);
   const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.taskbar);
+
+  const [pageNo, setPageNo] = useState(1);
+
+  const nextPage = () => {
+    if (pageNo !== 5) {
+      setPageNo(pageNo + 1);
+      console.log(pageNo);
+    }
+  };
 
   return (
     <div
@@ -25,8 +37,147 @@ export const Getstarted = () => {
       />
       <div className="windowScreen flex flex-col" data-dock="true">
         <div className="restWindow flex-grow flex flex-col">
-          <div className="flex-grow grid place-items-center text-3xl">
-            Coming soon? maybe
+          <div className="inner_fill_setup">
+            {pageNo === 1 ? (
+              <>
+                <div className="left">
+                  <img
+                    alt="left image"
+                    id="left_img"
+                    src="img/oobe/window11_oobe_region.png"
+                  />
+                </div>
+                <div className="right">
+                  <div className="header">
+                    Is this the right country or region? <br />
+                    <div className="header_sml"></div>
+                  </div>
+                  <div className="list_oobe mt-4 win11Scroll">
+                    {countries.map((e) => {
+                      return <div className="list_oobe_opt">{e}</div>;
+                    })}
+                  </div>
+                </div>
+              </>
+            ) : null}
+            {pageNo === 2 ? (
+              <>
+                <div className="left">
+                  <img
+                    id="left_img"
+                    src="img/oobe/window11_oobe_keyb_layout.png"
+                  />
+                </div>
+                <div className="right">
+                  <div className="header">
+                    Is this the right keyboard layout or input method?
+                    <div className="header_sml">
+                      If you also use another keyboard layout, you can add that
+                      next
+                    </div>
+                  </div>
+                  <div className="list_oobe mt-4 win11Scroll">
+                    {countries.map((e) => {
+                      return <div className="list_oobe_opt">{e}</div>;
+                    })}
+                  </div>
+                </div>
+              </>
+            ) : null}
+            {pageNo === 3 ? (
+              <>
+                <div className="left">
+                  <img id="left_img" src="img/oobe/window11_oobe_update.png" />
+                </div>
+                <div className="right align">
+                  <img id="loader" src="img/oobe/window11_oobe_region.png" />
+                  Checking for updates.
+                </div>
+              </>
+            ) : null}
+            {pageNo === 4 ? (
+              <>
+                <div className="left">
+                  <img id="left_img" src="img/oobe/window11_oobe_name.png" />
+                </div>
+                <div className="right">
+                  <div className="header mb-2">Let's name your PC</div>
+                  <div className="header_sml">
+                    Make it yours with unique name that's easy to recognize when
+                    connecting to it from other devices.Your Pc will restart
+                    after you name it.
+                  </div>
+                  <div className="OOBE_input">
+                    <input
+                      type="text"
+                      spellcheck="false"
+                      placeholder="Name"
+                      id="OOBE_input"
+                    />
+                  </div>
+                  <div className="text_sml_black">
+                    No more than 15 character <br />
+                    No spaces or any of the following special characters:
+                    <br />
+                    &quot;/\ [ ] : | &lt; &gt;+ = ; , ?
+                  </div>
+                </div>
+              </>
+            ) : null}
+            {pageNo === 5 ? (
+              <>
+                <div className="left">
+                  <img id="left_img" src="img/oobe/window11_oobe_wifi.png" />
+                </div>
+                <div className="right">
+                  <div className="header">
+                    Let's connect you to a network
+                    <div className="header_sml">
+                      You'll need an internet connection to continue the setting
+                      up your device.Once connected, you'll get the latest
+                      features and security updates.
+                    </div>
+                    <div className="ethernet_list">
+                      <div className="list_oobe_opt_wifi">
+                        <i id="connection" className="bx bx-desktop"></i>{" "}
+                        <div className="ethernet_list_opt_inr">
+                          <div className="text_sml_black_wifi">Ethernet 01</div>
+                          <div className="header_sml_wifi">Not connected</div>
+                        </div>
+                      </div>
+                      <div className="list_oobe_opt"></div>
+                      <div className="list_oobe_opt"></div>
+                    </div>
+                    <div className="text_sml_black">
+                      Having trouble to getting connected?
+                    </div>
+                    <div className="header_sml">
+                      For troubleshooting tips use another device and visit
+                      aka.ms/networksetup
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : null}
+
+            <div className="yes_button base" onClick={nextPage}>
+              Yes
+            </div>
+          </div>
+
+          <div className="setup_settings">
+            <img
+              alt="accessibility"
+              className="mr-4"
+              src="img/oobe/window11_oobe_accessibility.png"
+              width={16}
+            />
+            <Icon
+              className="taskIcon"
+              src={`audio${tasks.audio}`}
+              ui
+              width={16}
+            />
           </div>
         </div>
       </div>
--- src/reducers/apps.js
+++ src/reducers/apps.js
@@ -1,8 +1,8 @@
-import {allApps} from '../utils';
+import { allApps } from "../utils";
 
-var dev = ""
-if(process.env.REACT_APP_ENV=="development"){
-  dev = "taskmanager" // set the name (lowercase) of the app you are developing so that it will be opened on refresh
+var dev = "getstarted";
+if (process.env.REACT_APP_ENV == "development") {
+  dev = "taskmanager"; // set the name (lowercase) of the app you are developing so that it will be opened on refresh
 }
 
 const defState = {};
@@ -13,7 +13,7 @@ for (var i = 0; i < allApps.length; i++) {
   defState[allApps[i].icon].max = null;
   defState[allApps[i].icon].z = 0;
 
-  if(allApps[i].icon==dev){
+  if (allApps[i].icon == dev) {
     defState[allApps[i].icon].size = "mini";
     defState[allApps[i].icon].hide = false;
     defState[allApps[i].icon].max = true;
@@ -25,32 +25,32 @@ defState.hz = 2;
 
 const appReducer = (state = defState, action) => {
   var tmpState = { ...state };
-  if(action.type=="EDGELINK"){
-    var obj = {...tmpState["edge"]};
-    if(action.payload && action.payload.startsWith("http")){
+  if (action.type == "EDGELINK") {
+    var obj = { ...tmpState["edge"] };
+    if (action.payload && action.payload.startsWith("http")) {
       obj.url = action.payload;
-    }else if(action.payload && action.payload.length!=0){
-      obj.url = "https://www.bing.com/search?q="+action.payload;
-    }else{
+    } else if (action.payload && action.payload.length != 0) {
+      obj.url = "https://www.bing.com/search?q=" + action.payload;
+    } else {
       obj.url = null;
     }
 
     obj.size = "full";
     obj.hide = false;
     obj.max = true;
-    tmpState.hz+=1;
+    tmpState.hz += 1;
     obj.z = tmpState.hz;
     tmpState["edge"] = obj;
     return tmpState;
-  }else if(action.type=="SHOWDSK"){
+  } else if (action.type == "SHOWDSK") {
     var keys = Object.keys(tmpState);
 
     for (var i = 0; i < keys.length; i++) {
       var obj = tmpState[keys[i]];
-      if(obj.hide==false){
+      if (obj.hide == false) {
         obj.max = false;
-        if(obj.z==tmpState.hz){
-          tmpState.hz-=1;
+        if (obj.z == tmpState.hz) {
+          tmpState.hz -= 1;
         }
         obj.z = -1;
         tmpState[keys[i]] = obj;
@@ -58,21 +58,20 @@ const appReducer = (state = defState, action) => {
     }
 
     return tmpState;
-
-  }else if (action.type=="EXTERNAL") {
-    window.open(action.payload, '_blank');
-  }else if (action.type=="OPENTERM") {
-    var obj = {...tmpState["terminal"]};
+  } else if (action.type == "EXTERNAL") {
+    window.open(action.payload, "_blank");
+  } else if (action.type == "OPENTERM") {
+    var obj = { ...tmpState["terminal"] };
     obj.dir = action.payload;
 
     obj.size = "full";
     obj.hide = false;
     obj.max = true;
-    tmpState.hz+=1;
+    tmpState.hz += 1;
     obj.z = tmpState.hz;
     tmpState["terminal"] = obj;
     return tmpState;
-  }else if (action.type=="ADDAPP") {
+  } else if (action.type == "ADDAPP") {
     tmpState[action.payload.icon] = action.payload;
     tmpState[action.payload.icon].size = "full";
     tmpState[action.payload.icon].hide = true;
@@ -80,74 +79,74 @@ const appReducer = (state = defState, action) => {
     tmpState[action.payload.icon].z = 0;
 
     return tmpState;
-  }else if (action.type=="DELAPP") {
+  } else if (action.type == "DELAPP") {
     delete tmpState[action.payload];
     return tmpState;
-  }else{
+  } else {
     var keys = Object.keys(state);
     for (var i = 0; i < keys.length; i++) {
       var obj = state[keys[i]];
-      if(obj.action == action.type){
-        tmpState = {...state};
+      if (obj.action == action.type) {
+        tmpState = { ...state };
 
-        if(action.payload=="full"){
+        if (action.payload == "full") {
           obj.size = "full";
           obj.hide = false;
           obj.max = true;
-          tmpState.hz+=1;
+          tmpState.hz += 1;
           obj.z = tmpState.hz;
-        }else if(action.payload=="close"){
+        } else if (action.payload == "close") {
           obj.hide = true;
           obj.max = null;
           obj.z = -1;
-          tmpState.hz-=1;
-        }else if(action.payload=="mxmz"){
-          obj.size = ["mini","full"][obj.size!="full"?1:0];
+          tmpState.hz -= 1;
+        } else if (action.payload == "mxmz") {
+          obj.size = ["mini", "full"][obj.size != "full" ? 1 : 0];
           obj.hide = false;
           obj.max = true;
-          tmpState.hz+=1;
+          tmpState.hz += 1;
           obj.z = tmpState.hz;
-        }else if(action.payload=="togg"){
-          if(obj.z != tmpState.hz){
+        } else if (action.payload == "togg") {
+          if (obj.z != tmpState.hz) {
             obj.hide = false;
-            if(!obj.max){
-              tmpState.hz+=1;
+            if (!obj.max) {
+              tmpState.hz += 1;
               obj.z = tmpState.hz;
               obj.max = true;
-            }else{
+            } else {
               obj.z = -1;
               obj.max = false;
             }
-          }else{
+          } else {
             obj.max = !obj.max;
             obj.hide = false;
-            if(obj.max){
-              tmpState.hz+=1;
+            if (obj.max) {
+              tmpState.hz += 1;
               obj.z = tmpState.hz;
-            }else{
+            } else {
               obj.z = -1;
-              tmpState.hz-=1;
+              tmpState.hz -= 1;
             }
           }
-        }else if(action.payload=="mnmz"){
+        } else if (action.payload == "mnmz") {
           obj.max = false;
           obj.hide = false;
-          if(obj.z==tmpState.hz){
-            tmpState.hz-=1;
+          if (obj.z == tmpState.hz) {
+            tmpState.hz -= 1;
           }
           obj.z = -1;
-        }else if (action.payload=="resize"){
+        } else if (action.payload == "resize") {
           obj.size = "cstm";
           obj.hide = false;
           obj.max = true;
-          if(obj.z != tmpState.hz) tmpState.hz+=1;
+          if (obj.z != tmpState.hz) tmpState.hz += 1;
           obj.z = tmpState.hz;
           obj.dim = action.dim;
-        }else if (action.payload=="front") {
+        } else if (action.payload == "front") {
           obj.hide = false;
           obj.max = true;
-          if(obj.z!=tmpState.hz){
-            tmpState.hz+=1;
+          if (obj.z != tmpState.hz) {
+            tmpState.hz += 1;
             obj.z = tmpState.hz;
           }
         }
@@ -159,6 +158,6 @@ const appReducer = (state = defState, action) => {
   }
 
   return state;
-}
+};
 
 export default appReducer;
