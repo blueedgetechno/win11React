1353dabf0f7bb891b5cab6e674e98c58e12f74ca
Yashash Pugalia <yashash1511@gmail.com>
2022-05-24 12:11:27 +0530

    updates

--- src/containers/applications/apps/getstarted.js
+++ src/containers/applications/apps/getstarted.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ToolBar } from "../../../utils/general";
-import "./assets/getstarted.scss";
+import { Icon, ToolBar } from "../../../utils/general";
 import countries from "./assets/countrylist.json";
+import "./assets/getstarted.scss";
 
 export const Getstarted = () => {
   const apps = useSelector((state) => state.apps);
   const wnapp = useSelector((state) => state.apps.getstarted);
   const dispatch = useDispatch();
+
+  const tasks = useSelector((state) => state.taskbar);
 
   return (
     <div
@@ -26,41 +28,42 @@ export const Getstarted = () => {
       />
       <div className="windowScreen flex flex-col" data-dock="true">
         <div className="restWindow flex-grow flex flex-col">
-          <div class="white_blur_layer">
-            <div class="setup_settings">
+          <div class="inner_fill_setup">
+            <div class="yes_button base">Yes</div>
+            <div class="left">
               <img
-                alt="accessibility"
-                id="accessibility"
-                src="img/oobe/window11_oobe_accessibility.png"
-              />
-              <img
-                alt="volume"
-                id="volume"
-                src="img/oobe/window11_oobe_volume.png"
+                alt="left image"
+                id="left_img"
+                src="img/oobe/window11_oobe_region.png"
               />
             </div>
-            <div class="inner_fill_setup">
-              <div class="yes_button base">Yes</div>
-              <div class="left">
-                <img
-                  alt="left image"
-                  id="left_img"
-                  src="img/oobe/window11_oobe_region.png"
-                />
+            <div class="right">
+              <div class="header">
+                Is this the right country or region? <br />
+                <div class="header_sml"></div>
               </div>
-              <div class="right">
-                <div class="header">
-                  Is this the right country or region? <br />
-                  <div class="header_sml"></div>
-                </div>
 
-                <div class="list_oobe">
-                  {countries.map((e) => {
-                    return <div class="list_oobe_opt">{e}</div>;
-                  })}
-                </div>
+              <div class="list_oobe win11Scroll">
+                {countries.map((e) => {
+                  return <div class="list_oobe_opt">{e}</div>;
+                })}
               </div>
             </div>
+          </div>
+
+          <div class="setup_settings">
+            <img
+              alt="accessibility"
+              className="mr-8"
+              src="img/oobe/window11_oobe_accessibility.png"
+              width={16}
+            />
+            <Icon
+              className="taskIcon"
+              src={`audio${tasks.audio}`}
+              ui
+              width={16}
+            />
           </div>
         </div>
       </div>
