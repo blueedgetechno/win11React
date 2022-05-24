4dc73e0c52086eb96ea7303eb57714766c73189a
Yashash Pugalia <yashash1511@gmail.com>
2022-05-24 20:21:26 +0530

    les go

--- src/containers/applications/apps/getstarted.js
+++ src/containers/applications/apps/getstarted.js
@@ -5,7 +5,7 @@ import countries from "./assets/countrylist.json";
 import "./assets/getstarted.scss";
 import { useState } from "react";
 import LangSwitch from "./assets/Langswitch";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export const Getstarted = () => {
   const apps = useSelector((state) => state.apps);
@@ -16,15 +16,7 @@ export const Getstarted = () => {
 
   const [pageNo, setPageNo] = useState(1);
 
-  const nextPage = () => {
-    const lastPage = 6;
-    if (pageNo !== lastPage) {
-      setPageNo(pageNo + 1);
-      console.log(pageNo);
-    } else {
-    }
-  };
-
+  const nextPage = () => setPageNo(pageNo + 1);
   const userName = useSelector((state) => state.setting.person.name);
 
   return (
@@ -45,135 +37,165 @@ export const Getstarted = () => {
       <div className="windowScreen flex flex-col" data-dock="true">
         <div className="restWindow flex-grow flex flex-col">
           <div className="inner_fill_setup">
-            {pageNo === 1 ? (
-              <>
-                <div className="left">
-                  <img
-                    alt="left image"
-                    id="left_img"
-                    src="img/oobe/window11_oobe_region.png"
-                  />
-                </div>
-                <div className="right">
-                  <div className="header">
-                    {t('oobe.country')}<br />
-                    <div className="header_sml"></div>
-                  </div>
-                  <div className="list_oobe mt-4 win11Scroll">
-                    {countries.map((e) => {
-                      return <div className="list_oobe_opt">{e}</div>;
-                    })}
-                  </div>
-                </div>
-              </>
-            ) : null}
-            {pageNo === 2 ? (
-              <>
-                <div className="left">
-                  <img
-                    id="left_img"
-                    src="img/oobe/window11_oobe_keyb_layout.png"
-                  />
-                </div>
-                <div className="right">
-                  <div className="header">
-                    {t('oobe.keyboard')}
-                    <div className="header_sml">
-                      {t('oobe.anotherkeyboard')}
-                    </div>
-                  </div>
-                  <div className="list_oobe mt-4 win11Scroll">
-                    <LangSwitch />
-                  </div>
-                </div>
-              </>
-            ) : null}
-            {pageNo === 3 ? (
-              <>
-                <div className="left">
-                  <img id="left_img" src="img/oobe/window11_oobe_update.png" />
-                </div>
-                <div className="right align">
-                  <img id="loader" src="img/oobe/window11_oobe_region.png" />
-                  Checking for updates.
-                </div>
-              </>
-            ) : null}
-            {pageNo === 4 ? (
-              <>
-                <div className="left">
-                  <img id="left_img" src="img/oobe/window11_oobe_name.png" />
-                </div>
-                <div className="right">
-                  <div className="header mb-2">Let's name your PC</div>
-                  <div className="header_sml">
-                    Make it yours with unique name that's easy to recognize when
-                    connecting to it from other devices.Your Pc will restart
-                    after you name it.
-                  </div>
-                  <div className="OOBE_input">
-                    <input
-                      type="text"
-                      spellcheck="false"
-                      placeholder="name"
-                      id="OOBE_input"
-                    />
-                  </div>
-                  <div className="text_sml_black">
-                    No more than 15 character <br />
-                    No spaces or any of the following special characters:
-                    <br />
-                    &quot;/\ [ ] : | &lt; &gt;+ = ; , ?
-                  </div>
-                </div>
-              </>
-            ) : null}
-            {pageNo === 5 ? (
-              <>
-                <div className="left">
-                  <img id="left_img" src="img/oobe/window11_oobe_wifi.png" />
-                </div>
-                <div className="right">
-                  <div className="header">
-                    Let's connect you to a network
-                    <div className="header_sml">
-                      You'll need an internet connection to continue the setting
-                      up your device.Once connected, you'll get the latest
-                      features and security updates.
-                    </div>
-                    <div className="ethernet_list">
-                      <div className="list_oobe_opt_wifi">
-                        <i id="connection" className="bx bx-desktop"></i>{" "}
-                        <div className="ethernet_list_opt_inr">
-                          <div className="text_sml_black_wifi">Ethernet 01</div>
-                          <div className="header_sml_wifi">Not connected</div>
+            {(() => {
+              switch (pageNo) {
+                case 1:
+                  return (
+                    <>
+                      <div className="left">
+                        <img
+                          alt="left image"
+                          id="left_img"
+                          src="img/oobe/window11_oobe_region.png"
+                        />
+                      </div>
+                      <div className="right">
+                        <div className="header">
+                          {t("oobe.country")}
+                          <div className="header_sml mt-4"></div>
+                        </div>
+                        <div className="list_oobe mt-4 win11Scroll">
+                          {countries.map((e) => {
+                            return <div className="list_oobe_opt">{e}</div>;
+                          })}
                         </div>
                       </div>
-                      <div className="list_oobe_opt"></div>
-                      <div className="list_oobe_opt"></div>
-                    </div>
-                    <div className="text_sml_black">
-                      Having trouble to getting connected?
-                    </div>
-                    <div className="header_sml">
-                      For troubleshooting tips use another device and visit
-                      aka.ms/networksetup
-                    </div>
-                  </div>
-                </div>
-              </>
-            ) : null}
-            {pageNo === 6 ? (
-              <>
-                <div className="left">
-                  <img id="left_img" src="img/oobe/window11_oobe_update.png" />
-                </div>
-                <div className="right">
-                  <div className="header mb-8">The setup has completed.</div>
-                  <div>You can close this now.</div>
-                </div>
-              </>
-            ) : null}
+                    </>
+                  );
+                case 2:
+                  return (
+                    <>
+                      <div className="left">
+                        <img
+                          id="left_img"
+                          src="img/oobe/window11_oobe_keyb_layout.png"
+                        />
+                      </div>
+                      <div className="right">
+                        <div className="header">
+                          {t("oobe.keyboard")}
+                          <div className="header_sml">
+                            {t("oobe.anotherkeyboard")}
+                          </div>
+                        </div>
+                        <div className="list_oobe mt-4 win11Scroll">
+                          <LangSwitch />
+                        </div>
+                      </div>
+                    </>
+                  );
+                case 3:
+                  return (
+                    <>
+                      <div className="left">
+                        <img
+                          id="left_img"
+                          src="img/oobe/window11_oobe_update.png"
+                        />
+                      </div>
+                      <div className="right align">
+                        <img
+                          id="loader"
+                          src="img/oobe/window11_oobe_region.png"
+                        />
+                        Checking for updates.
+                      </div>
+                    </>
+                  );
+                case 4:
+                  return (
+                    <>
+                      <div className="left">
+                        <img
+                          id="left_img"
+                          src="img/oobe/window11_oobe_name.png"
+                        />
+                      </div>
+                      <div className="right">
+                        <div className="header mb-2">Let's name your PC</div>
+                        <div className="header_sml">
+                          Make it yours with unique name that's easy to
+                          recognize when connecting to it from other
+                          devices.Your Pc will restart after you name it.
+                        </div>
+                        <div className="OOBE_input">
+                          <input
+                            type="text"
+                            spellcheck="false"
+                            placeholder="name"
+                            id="OOBE_input"
+                          />
+                        </div>
+                        <div className="text_sml_black">
+                          No more than 15 character <br />
+                          No spaces or any of the following special characters:
+                          <br /> &quot;/\ [ ] : | &lt; &gt;+ = ; , ?
+                        </div>
+                      </div>
+                    </>
+                  );
+                case 5:
+                  return (
+                    <>
+                      <div className="left">
+                        <img
+                          id="left_img"
+                          src="img/oobe/window11_oobe_wifi.png"
+                        />
+                      </div>
+                      <div className="right">
+                        <div className="header">
+                          Let's connect you to a network
+                          <div className="header_sml">
+                            You'll need an internet connection to continue the
+                            setting up your device.Once connected, you'll get
+                            the latest features and security updates.
+                          </div>
+                          <div className="ethernet_list">
+                            <div className="list_oobe_opt_wifi">
+                              <i id="connection" className="bx bx-desktop"></i>{" "}
+                              <div className="ethernet_list_opt_inr">
+                                <div className="text_sml_black_wifi">
+                                  Ethernet 01
+                                </div>
+                                <div className="header_sml_wifi">
+                                  Not connected
+                                </div>
+                              </div>
+                            </div>
+                            <div className="list_oobe_opt"></div>
+                            <div className="list_oobe_opt"></div>
+                          </div>
+                          <div className="text_sml_black">
+                            Having trouble to getting connected?
+                          </div>
+                          <div className="header_sml">
+                            For troubleshooting tips use another device and
+                            visit aka.ms/networksetup
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  );
+                default:
+                  return (
+                    <>
+                      <div className="left">
+                        <img
+                          id="left_img"
+                          src="img/oobe/window11_oobe_update.png"
+                        />
+                      </div>
+                      <div className="right">
+                        <div className="header mb-8">
+                          The setup has completed.
+                        </div>
+                        <div>You can close this now.</div>
+                      </div>
+                    </>
+                  );
+              }
+            })()}
 
             <div className="yes_button base" onClick={nextPage}>
               Yes
