809832d906cd4faa6ff9d953a5f5072e92245a29
Lukas Batema <lukasbatema@gmail.com>
2022-05-30 13:03:17 -0400

    Fix deploying

--- src/components/taskbar/index.js
+++ src/components/taskbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Icon } from "../../utils/general";
 import Battery from "../Battery";
