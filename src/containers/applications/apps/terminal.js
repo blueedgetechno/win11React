e9c9f0ba268961f4d3c35c02300e487c10e49f1d
andrewstech <info@andrewstech.me>
2022-06-08 21:04:39 +0100

    derp

--- src/containers/applications/apps/terminal.js
+++ src/containers/applications/apps/terminal.js
@@ -3,6 +3,8 @@ import {useSelector, useDispatch} from 'react-redux';
 import i18next from 'i18next';
 import { GithubAuthProvider, OAuthCredential } from 'firebase/auth';
 import {useAuthState} from 'react-firebase-hooks/auth';
+import firebase from 'firebase/compat/app';
+
 
 
 
