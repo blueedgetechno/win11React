80061e094d67bd7269dff8ddba0b796badf5eb81
Yashash Pugalia <yashash1511@gmail.com>
2022-06-10 20:11:22 +0000

    fixes batteryStats not updating everywhere, better loading time

--- src/containers/background/index.js
+++ src/containers/background/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Battery from "../../components/Battery";
+import Battery from "../../components/shared/Battery";
 import { Icon, Image } from "../../utils/general";
 import "./back.scss";
 
