704aae97bb051df3b0039c471749619b04f7f177
BatemaDevelopment <lukasbatema@gmail.com>
2022-06-11 15:27:24 -0400

    Revert "Revert "fix batteryStats not updating everywhere, better loading time" (#522)"
    This reverts commit ac3691ba67911434909214f0ad569969e1e2f784.

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,82 +1,82 @@
-import {
-  desktopApps
-} from '../utils';
+import { desktopApps } from "../utils";
 
 const defState = {
   system: {
     power: {
       saver: {
-        state: false
-      }
+        state: false,
+      },
+      battery: 100,
     },
     display: {
       brightness: 100,
       nightlight: {
-        state: false
+        state: false,
       },
-      connect: false
-    }
+      connect: false,
+    },
   },
   person: {
     name: "Blue Edge",
     theme: "light",
-    color: "blue"
+    color: "blue",
   },
   devices: {
-    bluetooth: false
+    bluetooth: false,
   },
   network: {
     wifi: {
-      state: true
+      state: true,
     },
-    airplane: false
+    airplane: false,
   },
   privacy: {
     location: {
-      state: false
-    }
-  }
-}
+      state: false,
+    },
+  },
+};
 
-document.body.dataset.theme = defState.person.theme
+document.body.dataset.theme = defState.person.theme;
 
-const changeVal = (obj, path, val="togg")=>{
-  var tmp = obj
-  path = path.split(".")
+const changeVal = (obj, path, val = "togg") => {
+  var tmp = obj;
+  path = path.split(".");
   for (var i = 0; i < path.length - 1; i++) {
-    tmp = tmp[path[i]]
+    tmp = tmp[path[i]];
   }
 
-  if(val=="togg"){
-    tmp[path[path.length-1]] = !tmp[path[path.length-1]]
-  }else{
-    tmp[path[path.length-1]] = val
+  if (val == "togg") {
+    tmp[path[path.length - 1]] = !tmp[path[path.length - 1]];
+  } else {
+    tmp[path[path.length - 1]] = val;
   }
 
-  return obj
-}
+  return obj;
+};
 
 const settReducer = (state = defState, action) => {
-  var tmpState = {...state}, changed = false
+  var tmpState = { ...state },
+    changed = false;
   switch (action.type) {
-    case 'STNGTHEME':
-      changed = true
-      tmpState.person.theme = action.payload
-      break
-    case 'STNGTOGG':
-      changed = true
-      tmpState = changeVal(tmpState, action.payload)
-      break
-    case 'STNGSETV':
-      changed = true
-      tmpState = changeVal(tmpState,action.payload.path,action.payload.value)
-      break
-    case 'SETTLOAD':
-      tmpState = {...action.payload}
+    case "STNGTHEME":
+      changed = true;
+      tmpState.person.theme = action.payload;
+      break;
+    case "STNGTOGG":
+      changed = true;
+      tmpState = changeVal(tmpState, action.payload);
+      break;
+    case "STNGSETV":
+      changed = true;
+      tmpState = changeVal(tmpState, action.payload.path, action.payload.value);
+      break;
+    case "SETTLOAD":
+      tmpState = { ...action.payload };
   }
 
-  if(changed) localStorage.setItem("setting", JSON.stringify(tmpState))
+  if (changed) localStorage.setItem("setting", JSON.stringify(tmpState));
   return tmpState;
-}
+};
 
 export default settReducer;
