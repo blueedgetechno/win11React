e0831847a041bf2dc56bfde5cf22e69a17f0eaff
Posandu Mapa <posandumapa@gmail.com>
2022-07-14 23:58:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }) => {
     return defineConfig({
         plugins: [react()],
+        base: "",
         define: {
             "process.env.NODE_ENV": `"${mode}"`,
         },
