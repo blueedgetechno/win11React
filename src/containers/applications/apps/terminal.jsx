222cc6455e5e086c8e7bb3a111ca7f2195f9b6ba
unknown <matteo.messmer@gmail.com>
2022-10-09 12:36:11 +0200

    terminal color command

--- src/containers/applications/apps/terminal.jsx
+++ src/containers/applications/apps/terminal.jsx
@@ -112,6 +112,49 @@ export const WnTerminal = () => {
 			}
 		} else if (type == "cls") {
 			tmpStack = [];
+		} else if (type == "color") {
+			let color = "#FFFFFF";
+			let background = "#000000";
+			let re = /^[A-Fa-f0-9]+$/g;
+			if (!arg || (arg.length < 3 && re.test(arg))) {
+				if (arg.length == 2) {
+					color = colorCode(arg[1]);
+					background = colorCode(arg[0]);
+				} else if (arg.length == 1) {
+					color = colorCode(arg[0]);
+				}
+
+
+				//set background color of the element id cmdCont 
+				var cmdcont = document.getElementById("cmdcont");
+				cmdcont.style.backgroundColor = background;
+
+				//set color of text of .cmdLine class
+				cmdcont.style.color = color;
+
+			} else {
+				tmpStack.push("Set the color of the background and the text for the console.");
+				tmpStack.push("COLOR [arg]");
+				tmpStack.push("arg\t\tSpecifies the color for the console output");
+				tmpStack.push("The color attribute is a combination of the following values:");
+				tmpStack.push("0\t\tBlack");
+				tmpStack.push("1\t\tBlue");
+				tmpStack.push("2\t\tGreen");
+				tmpStack.push("3\t\tCyan");
+				tmpStack.push("4\t\tRed");
+				tmpStack.push("5\t\tMagenta");
+				tmpStack.push("6\t\tBrown");
+				tmpStack.push("7\t\tLight Gray");
+				tmpStack.push("8\t\tDark Gray");
+				tmpStack.push("9\t\tLight Blue");
+				tmpStack.push("A\t\tLight Green");
+				tmpStack.push("B\t\tLight Cyan");
+				tmpStack.push("C\t\tLight Red");
+				tmpStack.push("D\t\tLight Magenta");
+				tmpStack.push("E\t\tYellow");
+				tmpStack.push("F\t\tWhite");
+				tmpStack.push("Example: COLOR 0a for black text on a green background");
+			}
 		} else if (type == "type") {
 			var errp = true;
 
@@ -142,15 +185,15 @@ export const WnTerminal = () => {
 		} else if (type == "time") {
 			tmpStack.push(
 				"The current time is: " +
-					new Date()
-						.toLocaleTimeString("en-GB", {
-							hour: "2-digit",
-							minute: "2-digit",
-							second: "2-digit",
-						})
-						.replaceAll(":", ".") +
-					"." +
-					Math.floor(Math.random() * 100)
+				new Date()
+					.toLocaleTimeString("en-GB", {
+						hour: "2-digit",
+						minute: "2-digit",
+						second: "2-digit",
+					})
+					.replaceAll(":", ".") +
+				"." +
+				Math.floor(Math.random() * 100)
 			);
 		} else if (type == "exit") {
 			tmpStack = ["OS [Version 10.0.22000.51]", ""];
@@ -191,6 +234,7 @@ export const WnTerminal = () => {
 			var helpArr = [
 				"CD             Displays the name of or changes the current directory.",
 				"CLS            Clears the screen.",
+				"COLOR		  	Sets the default console foreground and background colors.",
 				"DATE           Displays or sets the date.",
 				"DIR            Displays a list of files and subdirectories in a directory.",
 				"ECHO           Displays messages, or turns command echoing on or off.",
@@ -221,6 +265,81 @@ export const WnTerminal = () => {
 
 		if (type.length > 0) tmpStack.push("");
 		setStack(tmpStack);
+	};
+
+	const colorCode = (color) => {
+		let code = "#000000";
+		/*
+			0: Black
+			1: Blue
+			2: Green
+			3: Cyan
+			4: Red
+			5: Magenta
+			6: Brown
+			7: Light Gray
+			8: Dark Gray
+			9: Light Blue
+			A: Light Green
+			B: Light Cyan
+			C: Light Red
+			D: Light Magenta
+			E: Yellow
+			F: White
+		*/
+
+		switch (color.toUpperCase()) {
+			case "0":
+				code = "#000000";
+				break;
+			case "1":
+				code = "#0000AA";
+				break;
+			case "2":
+				code = "#00AA00";
+				break;
+			case "3":
+				code = "#00AAAA";
+				break;
+			case "4":
+				code = "#AA0000";
+				break;
+			case "5":
+				code = "#AA00AA";
+				break;
+			case "6":
+				code = "#AA5500";
+				break;
+			case "7":
+				code = "#AAAAAA";
+				break;
+			case "8":
+				code = "#555555";
+				break;
+			case "9":
+				code = "#5555FF";
+				break;
+			case "A":
+				code = "#55FF55";
+				break;
+			case "B":
+				code = "#55FFFF";
+				break;
+			case "C":
+				code = "#FF5555";
+				break;
+			case "D":
+				code = "#FF55FF";
+				break;
+			case "E":
+				code = "#FFFF55";
+				break;
+			case "F":
+				code = "#FFFFFF";
+				break;
+		}
+
+		return code;
 	};
 
 	const action = (event) => {
