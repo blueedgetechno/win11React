3f89a65e979e48960e44e5a368ebd7180cc1f099
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-01-10 01:06:01 +0530

    remove old login page

--- src/api/model/login/google.ts
+++
@@ -1,7 +0,0 @@
-export class GoogleLoginForm {
-  constructor(token: string, validator: string) {
-    (this.Validator = validator), (this.token = token);
-  }
-  token: string;
-  Validator: string;
-}
