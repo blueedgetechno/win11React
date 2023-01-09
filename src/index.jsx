3cccaa42bf8610514f560022140df42181bc7ece
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-01-10 01:30:59 +0530

    remove firebase and sentry

--- src/index.jsx
+++ src/index.jsx
@@ -6,16 +6,16 @@ import App from "./App";
 import store from "./reducers";
 import { Provider } from "react-redux";
 
-Sentry.init({
-  dsn: "https://6c16d34365334e0fbee992044f9d223b@o575799.ingest.sentry.io/6251530",
-  integrations: [new BrowserTracing()],
+// Sentry.init({
+//   dsn: "https://6c16d34365334e0fbee992044f9d223b@o575799.ingest.sentry.io/6251530",
+//   integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  // environments for a more accurate trace.
-  tracesSampleRate: 1.0,
-});
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   // environments for a more accurate trace.
+//   tracesSampleRate: 1.0,
+// });
 
 const root = createRoot(document.getElementById("root"));
 
