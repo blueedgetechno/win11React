b23f67555fe0cba3b91a0ffcc89c4c18f7a46765
carosaome <sieunhankiet@gmail.com>
2023-04-24 04:20:10 +0000

    add link constant

---
+++ src/data/constant.js
@@ -0,0 +1,6 @@
+
+export const externalLink = {
+	DOCUMENT_LINK: 'https://thinkonmay.notion.site/Our-service-b6a51122ef8f4a998594576a9f3aed0f',
+	INTRODUCE_LINK: 'https://thinkonmay.notion.site/What-is-Thinkmay-57ef5071a5cc4d638584877f42957c34',
+	INTRODUCE_MEMBER_LINK: 'https://thinkonmay.notion.site/Team-member-f1060888766245dda40b3c46624a0223',
+}
