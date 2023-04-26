913a8bd552fc4056eb74e4abe68ee4d6ca1b397c
win11bot <w11bot@andrewstech.me>
2023-04-26 04:08:33 +0000

    Prettier
    skip actions

--- src/containers/applications/apps/about.jsx
+++ src/containers/applications/apps/about.jsx
@@ -25,28 +25,33 @@ export const AboutWin = () => {
           <div className="content p-6">
             <div className="text-xl font-semibold">{t("about.title")}</div>
             <code>
-              <strong> <a target="_blank" href={externalLink.INTRODUCE_MEMBER_LINK}> Thinkmay </a> </strong> 
-              is an organization was born with the ultimate goal is to{" "}
-              <i className="font-bold"> “change the way technology serve human and shape our knowledge about the world”.  </i>
-            </code>
-
-              <br />
-            <br />
-
-            <p className="my-2">
-                About this  {" "}
               <strong>
-                dashboard
+                {" "}
+                <a target="_blank" href={externalLink.INTRODUCE_MEMBER_LINK}>
+                  {" "}
+                  Thinkmay{" "}
+                </a>{" "}
               </strong>
-              </p>
-            1. For cloud PC user: 
+              is an organization was born with the ultimate goal is to{" "}
+              <i className="font-bold">
+                {" "}
+                “change the way technology serve human and shape our knowledge
+                about the world”.{" "}
+              </i>
+            </code>
+            <br />
+            <br />
+            <p className="my-2">
+              About this <strong>dashboard</strong>
+            </p>
+            1. For cloud PC user:
             <ul className="pl-5">
               <li>
                 {" "}
                 Go to <strong>Store</strong> to explore our vendor's services
               </li>
             </ul>
-            2. For our cloud PC vendor: 
+            2. For our cloud PC vendor:
             <ul className="pl-5">
               <li>
                 {" "}
