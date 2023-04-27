36ced4362ed0a61d07a1e370805d1adfc7cf5cab
carosaome <sieunhankiet@gmail.com>
2023-04-27 10:56:24 +0000

    catch err

--- src/supabase/function.js
+++ src/supabase/function.js
@@ -5,16 +5,19 @@ export const FetchAuthorizedWorkers = async () => {
   if (session.error != null) return new Error(session.error.message);
 
   const body = JSON.stringify({ use_case: "web" });
-  const { data, error } = await supabase.functions.invoke(
-    "worker_profile_fetch",
-    {
-      headers: { access_token: session.data.session.access_token },
-      body: body,
-      method: "POST",
-    }
-  );
-
-  return error == null ? data : new Error(error + ":" + data);
+  try {
+    const { data, error } = await supabase.functions.invoke(
+      "worker_profile_fetch",
+      {
+        headers: { access_token: session.data.session.access_token },
+        body: body,
+        method: "POST",
+      }
+    );
+    return error == null ? data : new Error(error + ":" + data);
+  } catch (error) {
+    return new Error(error);
+  }
 };
 
 export const DeactivateWorkerSession = async (worker_session_id) => {
@@ -25,16 +28,20 @@ export const DeactivateWorkerSession = async (worker_session_id) => {
     worker_session_id: worker_session_id,
   });
 
-  const { data, error } = await supabase.functions.invoke(
-    "worker_session_deactivate",
-    {
-      headers: { access_token: session.data.session.access_token },
-      body: body,
-      method: "POST",
-    }
-  );
+  try {
+    const { data, error } = await supabase.functions.invoke(
+      "worker_session_deactivate",
+      {
+        headers: { access_token: session.data.session.access_token },
+        body: body,
+        method: "POST",
+      }
+    );
 
-  return error == null ? data : new Error(error + ":" + data);
+    return error == null ? data : new Error(error + ":" + data);
+  } catch (error) {
+    return new Error(error)
+  }
 };
 
 export const CreateWorkerSession = async (worker_profile_id, media) => {
@@ -47,14 +54,18 @@ export const CreateWorkerSession = async (worker_profile_id, media) => {
     monitor_name: null,
   });
 
-  const { data, error } = await supabase.functions.invoke(
-    "worker_session_create",
-    {
-      headers: { access_token: session.data.session.access_token },
-      body: body,
-      method: "POST",
-    }
-  );
+  try {
+    const { data, error } = await supabase.functions.invoke(
+      "worker_session_create",
+      {
+        headers: { access_token: session.data.session.access_token },
+        body: body,
+        method: "POST",
+      }
+    );
 
-  return error == null ? data : new Error(error + ":" + data);
+    return error == null ? data : new Error(error + ":" + data);
+  } catch (error) {
+    return new Error(error)
+  }
 };
