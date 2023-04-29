97d9bc294e868cdc1739e09d113d15b884b63724
win11bot <w11bot@andrewstech.me>
2023-04-29 14:48:27 +0000

    Prettier
    skip actions

--- src/utils/formatData.js
+++ src/utils/formatData.js
@@ -1,57 +1,60 @@
 export function autoFormatData(data) {
   const newData = {
-    Account : {
-      type : "folder",
-      name : "Account",
-      info : {
-        size : "",
-        used : "",
-        spid : "%worker%"
+    Account: {
+      type: "folder",
+      name: "Account",
+      info: {
+        size: "",
+        used: "",
+        spid: "%worker%",
       },
-      data : {},
-    }
+      data: {},
+    },
   };
 
   data.tree.data.forEach((proxy) => {
     newData.Account.data[proxy.name] = {
-      type : "folder",
-      data : {},
-      info :{
+      type: "folder",
+      data: {},
+      info: {
         ...proxy.info,
-        menu : "proxy",
-      }, 
+        menu: "proxy",
+      },
     };
     proxy.data.forEach((worker) => {
       newData.Account.data[proxy.name].data[worker.name] = {
-        type : "folder",
-        data : {},
-        info : {
+        type: "folder",
+        data: {},
+        info: {
           ...worker.info,
-          menu : "worker",
-        }
+          menu: "worker",
+        },
       };
 
       worker.data.forEach((session) => {
-        newData.Account.data[proxy.name].data[worker.name].data[ session.name ] = {
-          type : "folder",
-          data : {},
-          info : { 
-            ...session.info,
-            menu : "session"
-          }
-        };
+        newData.Account.data[proxy.name].data[worker.name].data[session.name] =
+          {
+            type: "folder",
+            data: {},
+            info: {
+              ...session.info,
+              menu: "session",
+            },
+          };
         session.data.forEach((user_session) => {
-          newData.Account.data[proxy.name].data[worker.name].data[ session.name ].data[user_session.name] = {
-            type : "file",
-            data : {},
-            info : {
+          newData.Account.data[proxy.name].data[worker.name].data[
+            session.name
+          ].data[user_session.name] = {
+            type: "file",
+            data: {},
+            info: {
               ...user_session.info,
-              menu : "user"
+              menu: "user",
             },
           };
         });
       });
     });
-  })
+  });
   return newData;
 }
