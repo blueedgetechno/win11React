80d7471a45c118fece9fe32dca9ff4a7db218793
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-04-29 14:47:58 +0000

    change tree data

--- src/utils/formatData.js
+++ src/utils/formatData.js
@@ -1,72 +1,57 @@
 export function autoFormatData(data) {
-  const newData = {};
-  newData.Account = {};
-  newData.Account.type = "folder";
-  newData.Account.name = "Account";
-  newData.Account.info = {};
-  newData.Account.info.size = "";
-  newData.Account.info.used = "";
-  newData.Account.info.spid = "%worker%";
-  newData.Account.data = {};
-
-  for (const proxy of data.tree) {
-    newData.Account.data[proxy.name] = {};
-    newData.Account.data[proxy.name].type = "folder";
-    newData.Account.data[proxy.name].name = proxy.name;
-    newData.Account.data[proxy.name].info = proxy.info;
-    //newData.Account.data[proxy.name].info.spid = "%worker%";
-    newData.Account.data[proxy.name].info.menu = "proxy";
-    newData.Account.data[proxy.name].data = {};
-
-    if (proxy.data) {
-      proxy.data.forEach((worker) => {
-        newData.Account.data[proxy.name].data[worker.name] = {};
-        newData.Account.data[proxy.name].data[worker.name].type = "folder";
-        newData.Account.data[proxy.name].data[worker.name].name = worker.name;
-        newData.Account.data[proxy.name].data[worker.name].info = worker.info;
-        newData.Account.data[proxy.name].data[worker.name].info.menu = "worker";
-        newData.Account.data[proxy.name].data[worker.name].data = {};
-        if (worker.data) {
-          worker.data.forEach((session) => {
-            newData.Account.data[proxy.name].data[worker.name].data[
-              session.name
-            ] = {};
-            newData.Account.data[proxy.name].data[worker.name].data[
-              session.name
-            ].type = "folder";
-            newData.Account.data[proxy.name].data[worker.name].data[
-              session.name
-            ].data = {};
-            newData.Account.data[proxy.name].data[worker.name].data[
-              session.name
-            ].info = session.info;
-            newData.Account.data[proxy.name].data[worker.name].data[
-              session.name
-            ].info.menu = "session";
-
-            if (session.data) {
-              session.data.forEach((user_session) => {
-                newData.Account.data[proxy.name].data[worker.name].data[
-                  session.name
-                ].data[user_session.name] = {};
-                newData.Account.data[proxy.name].data[worker.name].data[
-                  session.name
-                ].data[user_session.name].info = user_session.info;
-                newData.Account.data[proxy.name].data[worker.name].data[
-                  session.name
-                ].data[user_session.name].info.menu = "user";
-                newData.Account.data[proxy.name].data[worker.name].data[
-                  session.name
-                ].data[user_session.name].type = "file";
-                newData.Account.data[proxy.name].data[worker.name].data[
-                  session.name
-                ].data[user_session.name].data = {};
-              });
-            }
-          });
-        }
-      });
+  const newData = {
+    Account : {
+      type : "folder",
+      name : "Account",
+      info : {
+        size : "",
+        used : "",
+        spid : "%worker%"
+      },
+      data : {},
     }
-  }
+  };
+
+  data.tree.data.forEach((proxy) => {
+    newData.Account.data[proxy.name] = {
+      type : "folder",
+      data : {},
+      info :{
+        ...proxy.info,
+        menu : "proxy",
+      }, 
+    };
+    proxy.data.forEach((worker) => {
+      newData.Account.data[proxy.name].data[worker.name] = {
+        type : "folder",
+        data : {},
+        info : {
+          ...worker.info,
+          menu : "worker",
+        }
+      };
+
+      worker.data.forEach((session) => {
+        newData.Account.data[proxy.name].data[worker.name].data[ session.name ] = {
+          type : "folder",
+          data : {},
+          info : { 
+            ...session.info,
+            menu : "session"
+          }
+        };
+        session.data.forEach((user_session) => {
+          newData.Account.data[proxy.name].data[worker.name].data[ session.name ].data[user_session.name] = {
+            type : "file",
+            data : {},
+            info : {
+              ...user_session.info,
+              menu : "user"
+            },
+          };
+        });
+      });
+    });
+  })
   return newData;
 }
