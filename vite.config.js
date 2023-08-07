b6544c50906d9e60080942048834f508c22e7890
carosaome <sieunhankiet@gmail.com>
2023-08-07 14:48:01 +0700

    add volume to cache

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const config = ({ mode }) => {
       VitePWA({
         registerType: "autoUpdate",
         workbox: {
-          maximumFileSizeToCacheInBytes: 6000000
+          maximumFileSizeToCacheInBytes: 12000000
         }
       }),
     ],
