7331e6329c58c7aa9d1b62609fa4bfa39da0944d
carosaome <sieunhankiet@gmail.com>
2023-08-07 10:38:24 +0700

    fix white screen

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ const config = ({ mode }) => {
       react(),
       VitePWA({
         registerType: "autoUpdate",
+        workbox: {
+          maximumFileSizeToCacheInBytes: 5000000
+        }
       }),
     ],
     base: "",
