9af048af0e4325663dc54d466eb92aa3466b6e19
carosaome <sieunhankiet@gmail.com>
2023-08-15 03:21:59 +0000

    dissable content front-page

--- src/containers/background/index.jsx
+++ src/containers/background/index.jsx
@@ -125,7 +125,7 @@ export const LockScreen = (props) => {
       style={{
         backgroundImage: `url(${`img/wallpaper/lock.jpg`})`,
       }}
-      //onClick={action}
+      onClick={action}
       //data-action="splash"
       data-blur={lock}
     >
@@ -145,49 +145,6 @@ export const LockScreen = (props) => {
           })}
         </div>
       </div>
-      {
-        lock == false && (<div className="text-gray-200 mt-[2em]">
-          <h2 className="text-center">Are you</h2>
-          <div className="flex gap-5 flex-wrap justify-center mt-[1em]">
-            <div className=" rounded-md bg-[#d9d9d954] pt-[16px] pb-[24px] px-[56px] cursor-pointer"
-              onClick={action}
-              data-action="splash"
-              data-user="B2C"
-            >
-              <h4 className="text-center">Want to</h4>
-              <p className="mt-[8px]">
-                <ReactTyped
-                  strings={["Play video game ", "Run blender ", "Run solid work "]}
-                  className="w-full text-center  inline-block font-bold"
-                  typeSpeed={100}
-                  loop
-                  backSpeed={20}
-                  showCursor={false}
-                />
-                <p className="w-full text-center">on all devices browser</p>
-              </p>
-            </div>
-            <div className=" rounded-md bg-[#d9d9d954] pt-[16px] pb-[24px] px-[56px] cursor-pointer"
-              onClick={action}
-              data-action="splash"
-              data-user="B2B"
-            >
-              <h4 className="text-center">Looking for</h4>
-              <div className="mt-[8px]">
-                <p className="ml-[3px]"><strong>Remote desktop</strong> solution for your</p>
-                <ReactTyped
-                  strings={["Render farm", "Cloud Gaming ", "Window cloud"]}
-                  className="w-full inline-block text-center"
-                  typeSpeed={100}
-                  loop
-                  backSpeed={20}
-                  showCursor={false}
-                />
-              </div>
-            </div>
-          </div>
-        </div>)
-      }
       <div className="fadeinScreen" data-faded={!lock} data-unlock={unlocked}>
         <Image
           className="rounded-2xl overflow-hidden"
