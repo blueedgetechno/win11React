76ec86fcd8fd23ee57c766025bdfab2d0de18a91
carosaome <sieunhankiet@gmail.com>
2023-08-15 10:23:12 +0000

    add tiengviet for error log

--- src/actions/fetch/index.js
+++ src/actions/fetch/index.js
@@ -1,3 +1,4 @@
+import i18next from "i18next";
 import { externalLink } from "../../data/constant";
 import supabase from "../../supabase/createClient";
 
@@ -117,7 +118,10 @@ const SupabaseFuncInvoke = async (funcName, options) => {
   }
 };
 
+const directDiscordMsg = ` Join <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support.`
 export const DownloadApplication = async (app_template_id) => {
+  let msg
+  const suggestMsg = i18next.t('error.run_out_of_gpu_stock');
   const { data, error } = await SupabaseFuncInvoke("request_application", {
     method: "POST",
     body: JSON.stringify({
@@ -126,30 +130,46 @@ export const DownloadApplication = async (app_template_id) => {
     }),
   });
   if (error != null) {
-    let msg = error;
+    msg = error;
     if (error === "run out of gpu stock") {
-      msg = "Hệ thông đang hết máy, bạn quay lại sau nhé.";
+      msg = i18next.t('error.run_out_of_gpu_stock');
     }
     throw `<p> 
               
               </br>
-              <b class='uppercase'>${msg}. Please reload and try it again, in fews minutes.</b> 
-              </br> Join 
-            <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support. 
+              <b class='uppercase'>${msg}. ${suggestMsg}</b> 
+              </br> 
+              ${directDiscordMsg} 
           <p>`;
   }
-  if (data == 'NOT_ALLOW') {
+
+  if (data.result == 'NOT_ALLOW') {
+    msg = i18next.t('error.NOT_ALLOW');
+
     throw `<p> 
               </br>
-              <b class='uppercase'>${msg}. Tài khoản này chưa đăng kí dịch vụ.</b> 
-              </br> Join 
-            <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support. 
+              <b class='uppercase'>${msg}</b> 
+              </br>               
+              ${directDiscordMsg} 
+
+          <p>`;
+  }
+  if (data.result == 'ALREADY_DEPLOYED') {
+    msg = i18next.t('error.ALREADY_DEPLOYED');
+    throw `<p> 
+              </br>
+              <b class='uppercase'>${msg}</b> 
+              </br>  
+              ${directDiscordMsg} 
           <p>`;
   }
   return data;
 };
 
 export const StartApplication = async (storage_id) => {
+  let msg
+  const suggestMsg = i18next.t('error.run_out_of_gpu_stock');
+
   const { data, error } = await SupabaseFuncInvoke("request_application", {
     method: "POST",
     body: JSON.stringify({
@@ -158,22 +178,23 @@ export const StartApplication = async (storage_id) => {
     }),
   });
   if (error != null) {
-    let msg = error;
+    msg = error;
     if (error === "run out of gpu stock") {
-      msg = "Hệ thông đang hết máy, bạn quay lại sau nhé.";
+      msg = i18next.t('error.run_out_of_gpu_stock');
     }
     throw `<p> 
-              
               </br>
-              <b class='uppercase'>${msg} Please reload and try it again, in fews minutes.</b> 
+              <b class='uppercase'>${msg}. ${suggestMsg}</b> 
               </br> Join 
-            <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support. 
+              ${directDiscordMsg} 
           <p>`;
   }
 
   return data;
 };
 export const AccessApplication = async (storage_id) => {
+  const suggestMsg = i18next.t('error.run_out_of_gpu_stock');
+
   const { data, error } = await SupabaseFuncInvoke("request_application", {
     method: "POST",
     body: JSON.stringify({
@@ -182,7 +203,12 @@ export const AccessApplication = async (storage_id) => {
     }),
   });
   if (error != null)
-    throw `<p> <br class='uppercase'>${error}. </br> Please reload and try it again, in fews minutes.</b> </br> Join <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support. <p>`;
+    throw `<p> <b class='uppercase'>${error}. 
+              </b>
+               ${suggestMsg}
+              </br> 
+              ${directDiscordMsg} 
+            <p>`;
   return data;
 };
 
@@ -203,6 +229,8 @@ export const DeleteApplication = async (storage_id) => {
 };
 
 export const StopApplication = async (storage_id) => {
+  const suggestMsg = i18next.t('error.run_out_of_gpu_stock');
+
   const { data, error } = await SupabaseFuncInvoke("request_application", {
     method: "POST",
     body: JSON.stringify({
@@ -211,7 +239,13 @@ export const StopApplication = async (storage_id) => {
     }),
   });
   if (error != null)
-    throw `<p> <b class='uppercase'>${error} Please reload and try it again, in fews minutes.</b> </br> Join <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support. <p>`;
+    throw `<p> 
+              <b class='uppercase'>${error}. 
+              ${suggestMsg}
+              </b> 
+              </br> 
+              ${directDiscordMsg} 
+            <p>`;
 
   return data;
 };
