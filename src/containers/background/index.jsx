748a8c2f883b6b4cc9da93709d2fece8fbc47092
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-08-19 08:03:47 +0700

    add payment button

--- src/containers/background/index.jsx
+++ src/containers/background/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { PayPalScriptProvider, PayPalButtons, FUNDING } from "@paypal/react-paypal-js";        
 import { useDispatch, useSelector } from "react-redux";
 import Battery from "../../components/shared/Battery";
 import { Icon, Image } from "../../utils/general";
@@ -115,6 +116,57 @@ export const LockScreen = (props) => {
     if (e.key == "Enter") proceed();
   };
 
+  const FUNDING_SOURCES = [
+    FUNDING.PAYPAL,
+    FUNDING.CARD
+  ];
+  
+  const initialOptions = {
+    "client-id": "AUGjxD_5EwowYxfVHGQSqtBsy0G7F05x850-iRLbbZZFTAZxYXn2ois63R1hZyA0ufbDch1I4lv9XUAZ",
+    "enable-funding": "",
+    "vault": true,
+  }
+
+  const payment = async (data, actions) => { try {
+      const response = await fetch(`http://localhost:9597/orders/${data.orderID}/capture`, {
+        method: "POST"
+      });             
+    
+      const details = await response.json();
+      // Three cases to handle:
+      //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+      //   (2) Other non-recoverable errors -> Show a failure message
+      //   (3) Successful transaction -> Show confirmation or thank you message
+
+      // This example reads a v2/checkout/orders capture response, propagated from the server
+      // You could use a different API or structure for your 'orderData'
+      const errorDetail = Array.isArray(details.details) && details.details[0];
+    
+      if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
+        return actions.restart();
+        // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
+      }
+
+      if (errorDetail) {
+        let msg = 'Sorry, your transaction could not be processed.';
+        msg += errorDetail.description ? ' ' + errorDetail.description : '';
+        msg += details.debug_id ? ' (' + details.debug_id + ')' : '';
+        alert(msg);
+      }
+      
+      // Successful capture! For demo purposes:
+      console.log('Capture result', details, JSON.stringify(details, null, 2));
+      const transaction = details.purchase_units[0].payments.captures[0];
+      alert('Transaction '+ transaction.status + ': ' + transaction.id + 'See console for all available details');
+    } catch (error) { console.error(error); }
+  }
+  
+  const sub = async (data, actions) => {
+    return actions.subscription.create({
+      plan_id: "P-3RX065706M3469222L5IFM4I",
+    });
+  }
+
   return (
     <div
       className={"lockscreen " + (props.dir == -1 ? "slowfadein" : "")}
@@ -155,6 +207,36 @@ export const LockScreen = (props) => {
         <div className="flex items-center mt-6 signInBtn" onClick={proceed}>
           {user.id ? " Enter" : "Sign In with GG"}
         </div>
+
+
+
+        <div className="mt-40">
+        {user.id ?  <PayPalScriptProvider options={initialOptions}> {
+          FUNDING_SOURCES.map(fundingSource=>{
+            return(
+              <PayPalButtons
+                fundingSource={fundingSource}
+                key={fundingSource}
+                
+                style={{
+                  layout: 'vertical',
+                  shape: 'pill',
+                  color: (fundingSource==FUNDING.PAYLATER) ? 'gold' : '',
+                }}
+
+                // createOrder={order}
+                createSubscription={sub}
+                onApprove={payment}
+            />)
+          })
+        
+        }
+        </PayPalScriptProvider>
+        : null}
+        </div>
+
+
+
         {/*<div>
           <input type={passType?"text":"password"} value={password} onChange={action}
               data-action="inpass" onKeyDown={action2} placeholder={passType?"Password":"PIN"}/>
