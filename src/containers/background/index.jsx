ff539e3a6b22cccdb1e4e632db45c0d7e0bea6a4
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-08-19 14:19:45 +0700

    update valid paypal

--- src/containers/background/index.jsx
+++ src/containers/background/index.jsx
@@ -77,6 +77,7 @@ export const LockScreen = (props) => {
   const dispatch = useDispatch();
 
   const user = useSelector((state) => state.user);
+  console.log(user)
 
   const userName = useSelector((state) => state.setting.person.name);
 
@@ -127,43 +128,13 @@ export const LockScreen = (props) => {
     "vault": true,
   }
 
-  const payment = async (data, actions) => { try {
-      const response = await fetch(`http://localhost:9597/orders/${data.orderID}/capture`, {
-        method: "POST"
-      });             
-    
-      const details = await response.json();
-      // Three cases to handle:
-      //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-      //   (2) Other non-recoverable errors -> Show a failure message
-      //   (3) Successful transaction -> Show confirmation or thank you message
-
-      // This example reads a v2/checkout/orders capture response, propagated from the server
-      // You could use a different API or structure for your 'orderData'
-      const errorDetail = Array.isArray(details.details) && details.details[0];
-    
-      if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
-        return actions.restart();
-        // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
-      }
-
-      if (errorDetail) {
-        let msg = 'Sorry, your transaction could not be processed.';
-        msg += errorDetail.description ? ' ' + errorDetail.description : '';
-        msg += details.debug_id ? ' (' + details.debug_id + ')' : '';
-        alert(msg);
-      }
-      
-      // Successful capture! For demo purposes:
-      console.log('Capture result', details, JSON.stringify(details, null, 2));
-      const transaction = details.purchase_units[0].payments.captures[0];
-      alert('Transaction '+ transaction.status + ': ' + transaction.id + 'See console for all available details');
-    } catch (error) { console.error(error); }
+  const payment = async (data, actions) => { 
+    console.log(data)
   }
   
   const sub = async (data, actions) => {
     return actions.subscription.create({
-      plan_id: "P-3RX065706M3469222L5IFM4I",
+      plan_id: "P-9KT21680D73416030MTQCQHI",
     });
   }
 
@@ -210,7 +181,7 @@ export const LockScreen = (props) => {
 
 
 
-        <div className="mt-40">
+        <div className="mt-40 items-center">
         {user.id ?  <PayPalScriptProvider options={initialOptions}> {
           FUNDING_SOURCES.map(fundingSource=>{
             return(
