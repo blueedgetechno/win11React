5981ba4b8c9065ecc5b91d75cabd1f1cfda911b9
epitchi <thienvanlea1@gmail.com>
2023-08-21 16:53:33 +0700

    + add upload store app

--- src/components/popup/modal/admin.jsx
+++ src/components/popup/modal/admin.jsx
@@ -12,7 +12,7 @@ const ModalEditOrInsert = (props) => {
       ? appData
       : {
           id: "",
-          title: "",
+          name: "",
           type: "",
           description: "",
           feature: "",
@@ -20,6 +20,10 @@ const ModalEditOrInsert = (props) => {
           icon: "",
         },
   );
+
+  const virtless_anon =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnY2t3anVja2xld3N1Y29jZmd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk2NzA5MTcsImV4cCI6MjAwNTI0NjkxN30.Ldcg3VJWf5fS5_SFmnfX2ZKHEfNoM9DPhoJFBStjjpA";
+ 
 
   const dispatch = useDispatch();
   const closeModal = async () => {
@@ -45,15 +49,15 @@ const ModalEditOrInsert = (props) => {
     const from = newFiles[0];
 
     const rand = crypto.randomUUID();
-    const randPath = `store/${formData.title}/${rand}`;
+    const randPath = `store/screenshot/${formData.name}/${rand}`;
 
     const uploadScreenShoot = await supabase.storage
-      .from("test")
+      .from("public_store")
       .upload(randPath, from);
     if (uploadScreenShoot.error) throw requestDb.error;
 
     const getScreenshootURL = await supabase.storage
-      .from("test")
+      .from("public_store")
       .getPublicUrl(randPath);
     if (getScreenshootURL.error) throw getScreenshootURL.error;
 
@@ -70,20 +74,20 @@ const ModalEditOrInsert = (props) => {
     if (event.target.files.length < 1) return;
 
     const rand = crypto.randomUUID();
-    const logoPath = `store/${formData.title}/${rand}`;
+    const logoPath = `store/${formData.type}/${formData.name}/${rand}`;
 
     const newFiles = Array.from(event.target.files);
     newFiles[0].link = URL.createObjectURL(newFiles[0]);
 
     const uploadLogo = await supabase.storage
-      .from("test")
+      .from("public_store")
       .upload(logoPath, newFiles[0], {
         upsert: true,
       });
     if (uploadLogo.error) throw uploadLogo.error;
 
     const getLogoID = await supabase.storage
-      .from("test")
+      .from("public_store")
       .getPublicUrl(logoPath);
     if (getLogoID.error) throw getLogoID.error;
 
@@ -121,45 +125,43 @@ const ModalEditOrInsert = (props) => {
   }
 
   async function handleInsertApp(newData) {
-    const { title, icon, description, type, feature, screenshoots } = newData;
+    const { name, icon, description, type, feature, screenshoots } = newData;
     
-    const virtless_anon =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnY2t3anVja2xld3N1Y29jZmd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk2NzA5MTcsImV4cCI6MjAwNTI0NjkxN30.Ldcg3VJWf5fS5_SFmnfX2ZKHEfNoM9DPhoJFBStjjpA";
     const resp = await fetch(
-      `https://dgckwjucklewsucocfgw.supabase.co/rest/v1/stores`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${virtless_anon}`,
-          apikey: virtless_anon,
-          // "refer": "return=minimal"
+        `https://dgckwjucklewsucocfgw.supabase.co/rest/v1/stores`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${virtless_anon}`,
+            apikey: virtless_anon,
+            // "refer": "return=minimal"
+          },
+          body: JSON.stringify({
+            "name": name,
+            "icon": icon,
+            "type": type,
+            "metadata": {
+              "description": description,
+              "feature": feature,
+              "screenshoots": screenshoots,
+            }
+          })
         },
-        body: JSON.stringify({
-          "name": title,
-          "icon": icon,
-          "type": type,
-          "metadata": {
-            "description": description,
-            "feature": feature,
-            "screenshoots": screenshoots,
-          }
-        })
-      },
-    );
+      );
 
-    if (resp.status != 200) throw await resp.text();
+      if (resp.status != 200) throw await resp.text();
   }
 
   const handleSubmitForm = async (event) => {
     event.preventDefault();
 
-    const title = formData.title;
+    const name = formData.name;
     const description = formData.description;
     const feature = formData.feature;
     const type = formData.type;
 
-    if (!title || !description || !type || !feature) {
+    if (!name || !description || !type || !feature) {
       log({ type: "error", content: "Fill in form." });
       return;
     }
@@ -181,16 +183,16 @@ const ModalEditOrInsert = (props) => {
         Name file have to english, have no space, no special character
       </h1>
       <div className="mb-4">
-        <label className="block text-gray-700 font-medium mb-2" htmlFor="title">
+        <label className="block text-gray-700 font-medium mb-2" htmlFor="name">
           Name
         </label>
         <input
           onChange={handleChangeInput}
-          value={formData.title}
+          value={formData.name}
           className="input w-full border-solid border border-gray-400 "
           type="text"
-          id="title"
-          name="title"
+          id="name"
+          name="name"
         />
       </div>
       <div className="mb-4">
