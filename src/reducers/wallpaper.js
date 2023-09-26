501db180879c6c3ba35549fcb8cac639dde91d71
Andrey Mironov <andreesssen@gmail.com>
2023-09-26 23:58:43 +0300

    fix:wallpaper keeps resetting #284

--- src/reducers/wallpaper.js
+++ src/reducers/wallpaper.js
@@ -1,106 +1,108 @@
-var wps = localStorage.getItem("wps") || 0;
-var locked = localStorage.getItem("locked");
+var wps = localStorage.getItem('wps') || 0
+var locked = localStorage.getItem('locked')
 
 const walls = [
-  "default/img0.jpg",
-  "dark/img0.jpg",
-  "ThemeA/img0.jpg",
-  "ThemeA/img1.jpg",
-  "ThemeA/img2.jpg",
-  "ThemeA/img3.jpg",
-  "ThemeB/img0.jpg",
-  "ThemeB/img1.jpg",
-  "ThemeB/img2.jpg",
-  "ThemeB/img3.jpg",
-  "ThemeC/img0.jpg",
-  "ThemeC/img1.jpg",
-  "ThemeC/img2.jpg",
-  "ThemeC/img3.jpg",
-  "ThemeD/img0.jpg",
-  "ThemeD/img1.jpg",
-  "ThemeD/img2.jpg",
-  "ThemeD/img3.jpg",
-];
+  'default/img0.jpg',
+  'dark/img0.jpg',
+  'ThemeA/img0.jpg',
+  'ThemeA/img1.jpg',
+  'ThemeA/img2.jpg',
+  'ThemeA/img3.jpg',
+  'ThemeB/img0.jpg',
+  'ThemeB/img1.jpg',
+  'ThemeB/img2.jpg',
+  'ThemeB/img3.jpg',
+  'ThemeC/img0.jpg',
+  'ThemeC/img1.jpg',
+  'ThemeC/img2.jpg',
+  'ThemeC/img3.jpg',
+  'ThemeD/img0.jpg',
+  'ThemeD/img1.jpg',
+  'ThemeD/img2.jpg',
+  'ThemeD/img3.jpg',
+]
 
-const themes = ["default", "dark", "ThemeA", "ThemeB", "ThemeD", "ThemeC"];
+const themes = ['default', 'dark', 'ThemeA', 'ThemeB', 'ThemeD', 'ThemeC']
 
 const defState = {
   themes: themes,
   wps: wps,
   src: walls[wps],
-  locked: !(locked == "false"),
-  booted: false || import.meta.env.MODE == "development",
-  act: "",
+  locked: !(locked == 'false'),
+  booted: false || import.meta.env.MODE == 'development',
+  act: '',
   dir: 0,
-};
+}
 
 const wallReducer = (state = defState, action) => {
   switch (action.type) {
-    case "WALLUNLOCK":
-      localStorage.setItem("locked", false);
+    case 'WALLUNLOCK':
+      localStorage.setItem('locked', false)
       return {
         ...state,
         locked: false,
         dir: 0,
-      };
-    case "WALLNEXT":
-      var twps = (state.wps + 1) % walls.length;
-      localStorage.setItem("wps", twps);
+      }
+    case 'WALLNEXT':
+      var twps = (state.wps + 1) % walls.length
+      localStorage.setItem('wps', twps)
       return {
         ...state,
         wps: twps,
         src: walls[twps],
-      };
-    case "WALLALOCK":
+      }
+    case 'WALLALOCK':
       return {
         ...state,
         locked: true,
         dir: -1,
-      };
-    case "WALLBOOTED":
+      }
+    case 'WALLBOOTED':
       return {
         ...state,
         booted: true,
         dir: 0,
-        act: "",
-      };
-    case "WALLRESTART":
+        act: '',
+      }
+    case 'WALLRESTART':
       return {
         ...state,
         booted: false,
         dir: -1,
         locked: true,
-        act: "restart",
-      };
-    case "WALLSHUTDN":
+        act: 'restart',
+      }
+    case 'WALLSHUTDN':
       return {
         ...state,
         booted: false,
         dir: -1,
         locked: true,
-        act: "shutdn",
-      };
-    case "WALLSET":
+        act: 'shutdn',
+      }
+    case 'WALLSET':
       var isIndex = !Number.isNaN(parseInt(action.payload)),
         wps = 0,
-        src = "";
+        src = ''
 
       if (isIndex) {
-        wps = action.payload;
-        src = walls[action.payload];
+        wps = localStorage.getItem('wps')
+        src = walls[wps] ? walls[wps] : walls[0]
       } else {
-        src = action.payload;
-        wps = walls.indexOf(action.payload);
+        const idx = walls.findIndex(item => item === action.payload)
+        localStorage.setItem('wps', idx)
+        src = action.payload
+        wps = walls[idx]
       }
 
       return {
         ...state,
         wps: wps,
         src: src,
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default wallReducer;
+export default wallReducer
