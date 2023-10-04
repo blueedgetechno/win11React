328023e97b508184dacc826cf5109885f734ba84
carosaome <sieunhankiet@gmail.com>
2023-10-04 15:06:28 +0700

    change the way open app

--- src/actions/remote.js
+++ src/actions/remote.js
@@ -1,4 +1,11 @@
+import { sleep } from "../utils/sleep";
+
 export function openRemotePage(url) {
     const open = `${url}&turn=true&no_stretch=true`
-    window.open(open, "_blank");
+    var windowReference = window.open();
+    //window.open(open, "_blank");
+
+    return sleep(100).then(function (url) {
+        windowReference.location = open;
+    });
 }
