234146003e9c60fa7aa29bf439ba8e8497196185
carosaome <sieunhankiet@gmail.com>
2023-10-04 16:56:00 +0700

    change open app

--- src/actions/remote.js
+++ src/actions/remote.js
@@ -2,10 +2,13 @@ import { sleep } from "../utils/sleep";
 
 export function openRemotePage(url) {
     const open = `${url}&turn=true&no_stretch=true`
-    var windowReference = window.open();
-    //window.open(open, "_blank");
+    //var windowReference = window.open();
+    ////window.open(open, "_blank");
 
-    return sleep(100).then(function (url) {
-        windowReference.location = open;
-    });
+    //return sleep(100).then(function (url) {
+    //    windowReference.location = open;
+    //});
+    setTimeout(() => {
+        window.open(open, '_blank');
+    }, 0);
 }
