d47692880e3c6a5ce7536eea60a0be87e2015432
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-10-07 21:40:16 +0700

    high priority process

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,14 @@
   windows_subsystem = "windows"
 )]
 
+use windows_sys::{
+  Win32::System::Threading::*,
+};
+
 fn main() {
+  unsafe {
+    SetPriorityClass( GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+  }
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
