3a116ca67d14fe72ebda2e8b87b6d19615fa47ea
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-10-12 07:27:18 +0000

    fix dump commit

--- src/actions/preload.js
+++ src/actions/preload.js
@@ -143,7 +143,8 @@ export const fetchStore = async () => {
   );
   if (resp.status != 200) 
     throw await resp.text();
-  const data = await resp.json().filter(e => e.hide != true);
+  const data = (await resp.json())
+    .filter(e => e.hide != true);
 
   const content = {
     games: [],
