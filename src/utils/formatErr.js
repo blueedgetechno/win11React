c88304be1f19b29e9f731c125ee772e6f52587a7
carosaome <sieunhankiet@gmail.com>
2023-11-09 13:37:34 +0700

    func format err

--- src/utils/formatErr.js
+++ src/utils/formatErr.js
@@ -1,12 +1,47 @@
 import i18next from "i18next";
 import { externalLink } from "../data/constant";
+import { log } from "../lib/log";
 
-export function formatError(err = 'Something went wrong!', code = '0') {
+const listErr = [
+	{
+		msg: 'ran out of hardware',
+		text: "error.run_out_of_gpu_stock"
+	},
+	{
+		msg: 'ran out of gpu',
+		text: "error.run_out_of_gpu_stock"
+	},
+	{
+		msg: 'is locked',
+		text: "error.IS_LOCKED"
+	},
+	{
+		msg: 'cluster not exist or not active', //TODO
+		text: "Server is down!"
+	}
+]
+const includesErr = (err = '') => {
+	let errFormat = ''
+	console.log(err.toString());
+	for (let i = 0; i < listErr.length; i++) {
+
+		if (err?.toString()?.includes(listErr[i].msg)) {
+			errFormat = i18next.t(listErr[i].text)
+			break;
+		}
+
+	}
+
+	console.log(errFormat);
+	return errFormat
+}
+export async function formatError(err = 'Something went wrong!', code = '0') {
 
 	const suggestMsg = i18next.t("error.suggest");
 	const directDiscordMsg = ` Join <a target='_blank' href=${externalLink.DISCORD_LINK}>Thinkmay Discord</a> for support.`;
 
 	let msg
+	let icon = code != 0 ? 'info' : 'error'
 
 	const CAUSES = {
 		"0": err,
@@ -20,11 +55,10 @@ export function formatError(err = 'Something went wrong!', code = '0') {
 
 
 	msg = CAUSES[code] ?? err
-	if (err.includes('ran out of hardware') || err.includes('ran out of gpu')) {
-		msg = i18next.t("error.run_out_of_gpu_stock")
-	}
-	if (err.includes('is locked')) {
-		msg = i18next.t("error.IS_LOCKED")
+
+	if (includesErr(err) != '') {
+		msg = includesErr(err)
+		icon = 'info'
 	}
 	const template = `<p> <b class='uppercase'>${msg}. </b>
 						${suggestMsg}
@@ -32,6 +66,13 @@ export function formatError(err = 'Something went wrong!', code = '0') {
 						${directDiscordMsg} 
 					  <p>`
 
+
+	await log({
+		type: "error",
+		icon: icon,
+		title: icon == 'info' ? '' : icon,
+		content: template,
+	});
 
 
 	return template
