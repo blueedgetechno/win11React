ef2ac96ee1620aec60b659a184e4333664203dc0
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-12-13 16:55:17 +0700

    remove unused scripts

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,8 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './backend/reducers';
-
-// Sentry.init({
-//   dsn: "https://6c16d34365334e0fbee992044f9d223b@o575799.ingest.sentry.io/6251530",
-//   integrations: [new BrowserTracing()],
-
-//   // Set tracesSampleRate to 1.0 to capture 100%
-//   // of transactions for performance monitoring.
-//   // We recommend adjusting this value in production
-//   // environments for a more accurate trace.
-//   tracesSampleRate: 1.0,
-// });
 
 const root = createRoot(document.getElementById('root'));
 
