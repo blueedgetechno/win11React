851b16d0ecd087c259f9b7a97fcd1a5a65116335
carosaome <sieunhankiet@gmail.com>
2023-12-19 12:09:21 +0700

    fix notify

--- src/backend/utils/forrmatNotify.ts
+++ src/backend/utils/forrmatNotify.ts
@@ -5,8 +5,8 @@ type Type = 'pending' | 'success' | 'rejected';
 export const formatTitleNotify = (type: Type, action: string) => {
     console.log(type, action);
     let prefix = i18next.t(`notify.status.${type}`);
-    let content = i18next.t(`notify.status.${action}`);
-    let subfix = i18next.t('info.startApp');
+    let content = i18next.t(`notify.action.${action}`);
+    let subfix = ''
 
     console.log(subfix);
 
