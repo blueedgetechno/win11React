4826005f1fadf02412f7ce43d16c9d6b5e721e85
carosaome <sieunhankiet@gmail.com>
2023-12-21 15:53:44 +0700

    format err

--- src/backend/utils/formatErr.ts
+++ src/backend/utils/formatErr.ts
@@ -15,12 +15,17 @@ map.set(CAUSE.NEED_WAIT, 'need_wait'); //
 map.set(CAUSE.INVALID_REQUEST, 'invalid_request');
 
 export function formatError(error: Error) {
+    let formatMsg
     console.log(error);
     const err = JSON.parse(error.message) as {
         message: string;
         code: CAUSE;
     };
-    return map.get(err.code) ?? includesErr(err.message);
+    formatMsg = map.get(err.code)
+    if (err.code == 0 || !map.get(err.code)) {
+        formatMsg = includesErr(err.message)
+    }
+    return formatMsg
 }
 
 const listErr = [
