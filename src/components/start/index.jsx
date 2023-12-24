85e63969c18c74abdcb4bdcf2626a5abd6330bb3
syedbarimanjan <syedbarimanjan@gmail.com>
2023-12-24 12:39:11 +0500

    perf(ui): change the desktop icons to open on double click.

--- src/components/start/index.jsx
+++ src/components/start/index.jsx
@@ -45,6 +45,24 @@ export const DesktopApp = () => {
     return arr;
   });
   const dispatch = useDispatch();
+  const sidepane = useSelector((state) => state.sidepane);
+	const doubleClickDispatch = (event) => {
+		if (!sidepane.banhide) dispatch({ type: "BANDHIDE" });
+		var action = {
+			type: event.currentTarget.dataset.action,
+			payload: event.currentTarget.dataset.payload,
+		};
+
+		if (action.type) {
+			dispatch(action);
+		}
+			event.currentTarget.dataset.pr = true;
+			event.currentTarget.dataset.click = true;
+	};
+	const clickDispatch = (event) =>{
+			event.currentTarget.dataset.pr = false;
+			event.currentTarget.dataset.click = false;
+	}
 
   return (
     <div className="desktopCont">
@@ -52,7 +70,15 @@ export const DesktopApp = () => {
         deskApps.apps.map((app, i) => {
           return (
             // to allow it to be focusable (:focus)
-            <div key={i} className="dskApp" tabIndex={0}>
+            <div 
+              key={i} 
+              className="dskApp" 
+              tabIndex={0}
+              onDoubleClick={doubleClickDispatch}
+              onClick={clickDispatch}
+              data-action={app.action}
+              data-payload={app.payload || "full"}
+            >
               <Icon
                 click={app.action}
                 className="dskIcon prtclk"
