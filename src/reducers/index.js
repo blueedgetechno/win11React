49f80c1fc587688b46d05756389fb7142fb1174e
syedbarimanjan <syedbarimanjan@gmail.com>
2023-12-24 12:35:22 +0500

    build: add redux devtools

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import wallReducer from "./wallpaper";
 import taskReducer from "./taskbar";
@@ -26,6 +27,6 @@ const allReducers = combineReducers({
   files: fileReducer,
 });
 
-var store = createStore(allReducers);
+var store = createStore(allReducers,devToolsEnhancer());
 
 export default store;
