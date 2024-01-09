a508f103b30df0cdd5dc0d3b31ca395c84c8d6bd
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-01-09 14:34:33 +0700

    transaction page

--- src/containers/applications/apps/payment.jsx
+++ src/containers/applications/apps/payment.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { appDispatch, useAppSelector } from '../../../backend/reducers';
+import { Contents } from '../../../backend/reducers/locales';
 import { LazyComponent, ToolBar } from '../../../components/shared/general';
 import './assets/store.scss';
 
@@ -33,6 +34,7 @@ export const PaymentApp = () => {
             highlight: true,
             title: 'Start',
             price: '12',
+
 
             name: 'month',
         },
@@ -71,24 +73,9 @@ export const PaymentApp = () => {
         // );
     };
 
-    const payment = async (data, actions) => { };
-
-    const subscribe = async (user, plan_id, actions) => {
-        return actions.subscription.create({
-            plan_id: plan_id,
-            custom_id: user.id
-        });
-    };
-
-    const handlePayment = ({ type, price }) => {
-        return () => {
-            const userEmail = user?.email || 'admin@gmail.com';
-            const userName = user?.user_metadata.name || 'userEmail';
-            dispatch({
-                type: 'PM_MODAL',
-                payload: { type, price, userEmail, userName }
-            });
-        };
+    const [paypage, setPaypage] = useState(false)
+    const payment = async (data, actions) => {
+        setPaypage(true)
     };
 
     return (
@@ -111,78 +98,223 @@ export const PaymentApp = () => {
             />
             <div className="windowScreen">
                 <LazyComponent show={!wnapp.hide}>
-                    <div class="paymentContent ">
-                        {ListSubs.map((sub, index) => (
-                            <div key={index} className="sub">
+                    {paypage
+                        ? <Payment onClose={() => setPaypage(false)}/>
+                        : <div class="paymentContent ">
+                            {ListSubs.map((sub, index) => (
+                                <div key={index} className="sub">
 
-                                {
-                                    sub.highlight
-                                        ? <p class="text-[13px] leading-4 text-center py-2 text-background">Most Popular</p>
-                                        : null
-                                }
+                                    {
+                                        sub.highlight
+                                            ? <p class="text-[13px] leading-4 text-center py-2 text-background">Most Popular</p>
+                                            : null
+                                    }
 
-                                <div class="flex flex-col overflow-hidden border h-full rounded-[4px]">
-                                    <div class="bg-surface-100 px-8 xl:px-4 2xl:px-8 pt-6 rounded-tr-[4px] rounded-tl-[4px] ">
-                                        <div class="mb-2 flex items-center gap-2">
-                                            <div class="flex items-center gap-2">
-                                                <h3 class="text-brand-600 text-2xl font-normal uppercase flex items-center gap-4 font-mono">
-                                                    {sub.name}
-                                                </h3>
+                                    <div class="flex flex-col overflow-hidden border h-full rounded-[4px]">
+                                        <div class="bg-surface-100 px-8 xl:px-4 2xl:px-8 pt-6 rounded-tr-[4px] rounded-tl-[4px] ">
+                                            <div class="mb-2 flex items-center gap-2">
+                                                <div class="flex items-center gap-2">
+                                                    <h3 class="text-brand-600 text-2xl font-normal uppercase flex items-center gap-4 font-mono">
+                                                        {sub.name}
+                                                    </h3>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <p class="text-foreground-light my-4 text-sm border-b border-default pb-4 2xl:pr-4">
-                                            Perfect for passion projects &amp; simple websites.
-                                        </p>
+                                            <p class="text-foreground-light my-4 text-sm border-b border-default pb-4 2xl:pr-4">
+                                                Perfect for passion projects &amp; simple websites.
+                                            </p>
 
-                                        <div class=" text-foreground flex items-baseline text-5xl font-normal lg:text-4xl xl:text-4xl border-b border-default min-h-[175px] pt-10">
-                                            <div class="flex flex-col gap-1">
-                                                <div class="flex items-end gap-2">
-                                                    <div>
-                                                        <div class="flex items-end">
-                                                            <p class="mt-2 gradient-text-500 pb-1 text-5xl">${sub.price}</p>
-                                                            <p class="text-foreground-lighter mb-1.5 ml-1 text-[13px] leading-4">/ month / org</p>
+                                            <div class=" text-foreground flex items-baseline text-5xl font-normal lg:text-4xl xl:text-4xl border-b border-default min-h-[175px] pt-10">
+                                                <div class="flex flex-col gap-1">
+                                                    <div class="flex items-end gap-2">
+                                                        <div>
+                                                            <div class="flex items-end">
+                                                                <p class="mt-2 gradient-text-500 pb-1 text-5xl">${sub.price}</p>
+                                                                <p class="text-foreground-lighter mb-1.5 ml-1 text-[13px] leading-4">/ month / org</p>
+                                                            </div>
+                                                            <p class="-mt-2">
+                                                                <span class="bg-background text-brand-600 border shadow-sm rounded-md bg-opacity-30 py-0.5 px-2 text-[13px] leading-4">
+                                                                    Limit of 2 free organizations
+                                                                </span>
+                                                            </p>
                                                         </div>
-                                                        <p class="-mt-2">
-                                                            <span class="bg-background text-brand-600 border shadow-sm rounded-md bg-opacity-30 py-0.5 px-2 text-[13px] leading-4">
-                                                                Limit of 2 free organizations
-                                                            </span>
-                                                        </p>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
 
-                                    <div class="border-default bg-surface-100 flex h-full rounded-bl-[4px] rounded-br-[4px] flex-1 flex-col px-8 xl:px-4 2xl:px-8 py-6 ">
-                                        <p class="text-foreground-light text-[13px] mt-2 mb-4">Get started with:</p>
-                                        <ul role="list" class="text-[13px] text-foreground-lighter">
-                                            <li class="flex items-center py-2 first:mt-0">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="sbui-icon text-brand h-4 w-4" aria-hidden="true">
-                                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                                </svg>
-                                                <span class="text-foreground mb-0 ml-3 ">Unlimited API requests</span>
-                                            </li>
-                                        </ul>
-                                        <div class="flex flex-col gap-6 mt-auto prose">
-                                            <div class="space-y-2 mt-12">
-                                                <p class="text-[13px] whitespace-pre-wrap">Free projects are paused after 1 week of inactivity.</p>
+                                        <div class="border-default bg-surface-100 flex h-full rounded-bl-[4px] rounded-br-[4px] flex-1 flex-col px-8 xl:px-4 2xl:px-8 py-6 ">
+                                            <p class="text-foreground-light text-[13px] mt-2 mb-4">Get started with:</p>
+                                            <ul role="list" class="text-[13px] text-foreground-lighter">
+                                                <li class="flex items-center py-2 first:mt-0">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="sbui-icon text-brand h-4 w-4" aria-hidden="true">
+                                                        <polyline points="20 6 9 17 4 12"></polyline>
+                                                    </svg>
+                                                    <span class="text-foreground mb-0 ml-3 ">Unlimited API requests</span>
+                                                </li>
+                                            </ul>
+                                            <div class="flex flex-col gap-6 mt-auto prose">
+                                                <div class="space-y-2 mt-12">
+                                                    <p class="text-[13px] whitespace-pre-wrap">Free projects are paused after 1 week of inactivity.</p>
+                                                </div>
+
+                                                <a href="javascript:void(0)" onClick={payment}>
+                                                    <button type="button" class="relative cursor-pointer space-x-2 text-center font-regular ease-out duration-200 rounded-md outline-none transition-all outline-0 focus-visible:outline-4 focus-visible:outline-offset-1 border bg-brand-button hover:bg-brand-button/80 text-white border-brand focus-visible:outline-brand-600 shadow-sm w-full flex items-center justify-center text-sm leading-4 px-3 py-2 bg-black">
+                                                        <span class="truncate">
+                                                            Get Started
+                                                        </span>
+                                                    </button>
+                                                </a>
                                             </div>
-
-                                            <a href="javascript:void(0)" onClick={payment}>
-                                                <button type="button" class="relative cursor-pointer space-x-2 text-center font-regular ease-out duration-200 rounded-md outline-none transition-all outline-0 focus-visible:outline-4 focus-visible:outline-offset-1 border bg-brand-button hover:bg-brand-button/80 text-white border-brand focus-visible:outline-brand-600 shadow-sm w-full flex items-center justify-center text-sm leading-4 px-3 py-2 bg-black">
-                                                    <span class="truncate">
-                                                        Get Started
-                                                    </span>
-                                                </button>
-                                            </a>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    }
                 </LazyComponent>
             </div>
-        </div>
+        </div >
     )
+};
+
+
+
+const Payment = ({onClose}) => {
+    const t = useAppSelector((state) => state.globals.translation);
+    const [result, SetResult] = useState([]);
+    const Survey = useAppSelector((state) => state.sidepane.surveys);
+    const { email, id } = useAppSelector((state) => state.user);
+
+    const [pageNo, setPageNo] = useState(0);
+    const nextPage = () =>
+        setPageNo((old) => {
+            const current = pages.at(old);
+            if (current && current.guidance)
+                SetResult((old) => [
+                    ...old,
+                    {
+                        question: current.data.question,
+                        selection: current.data.options.at(selection)
+                    }
+                ]);
+
+            return old + 1;
+        });
+    const prevPage = () =>
+        setPageNo((old) => {
+            const n = old != 0 ? old - 1 : old;
+            const current = pages.at(n);
+            if (current && current.guidance)
+                SetResult((old) => {
+                    old.pop();
+                    return old;
+                });
+
+            return n;
+        });
+    const reportSurvey = async () => {
+        await supabase.from('generic_events').insert({
+            value: result,
+            name: `survey result from ${email}`,
+            type: 'SURVEY'
+        });
+    };
+
+    const finishSurvey = async () => {
+        // await reportSurvey();
+        onClose()
+    };
+
+    const [selection, Select] = useState(0);
+    useEffect(() => {
+        const handle = (e) =>
+            e.key == 'Enter'
+                ? nextPage()
+                : e.key == 'ArrowUp'
+                    ? Select((old) => old - 1)
+                    : e.key == 'ArrowLeft'
+                        ? prevPage()
+                        : e.key == 'ArrowRight'
+                            ? nextPage()
+                            : e.key == 'ArrowDown'
+                                ? Select((old) => old + 1)
+                                : null;
+        window.addEventListener('keydown', handle);
+        return () => {
+            window.removeEventListener('keydown', handle);
+        };
+    }, []);
+
+    const Finish = () => (
+        <>
+            <div className="yes_button base" onClick={finishSurvey}>
+                Continue
+            </div>
+        </>
+    );
+
+    const Navigate = () => (
+        <>
+            <div className="no_button base" onClick={prevPage}>
+                Back
+            </div>
+            <div className="yes_button base" onClick={nextPage}>
+                Next
+            </div>
+        </>
+    );
+
+    const Logo = () => (
+        <div className="left">
+            <img alt="left image" id="left_img" src="logo_white.png" />
+        </div>
+    );
+
+    const pages = Survey.map((x, i) => { });
+
+    pages.unshift({
+        survey: false,
+        content: (
+            <>
+                <Logo />
+                <div className="right">
+                    <div className="header mb-8">
+                        We will follow up with the payment process
+                    </div>
+                    <div>
+                        We will follow up with the payment process
+                        <br />
+                        <br />
+                        We will follow up with the payment process
+                        <br />
+                    </div>
+                </div>
+                <Navigate/>
+            </>
+        )
+    });
+
+    pages.push({
+        content: (
+            <>
+                <Logo />
+                <div className="right">
+                    <div className="header mb-8">{'Thank you'}</div>
+                    <Finish />
+                </div>
+            </>
+        )
+    });
+
+    return (
+        <div className="getstarted floatTab dpShad" >
+            <div className="windowScreen flex flex-col" data-dock="true">
+                <div className="restWindow flex-grow flex flex-col p-[24px]">
+                    <div className="inner_fill_setup">
+                        {pages.at(pageNo)?.content}
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
 };
