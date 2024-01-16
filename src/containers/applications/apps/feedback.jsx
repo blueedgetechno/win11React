c316fedcc062e6f50573c293b1d8e02e08947a6f
epitchi <thienvanlea1@gmail.com>
2024-01-16 11:38:03 +0700

    + ui feedback, qa

--- src/containers/applications/apps/feedback.jsx
+++ src/containers/applications/apps/feedback.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { detectBrowserAndOS } from '../../../backend/utils/detectBrower';
-import { appDispatch, app_close, app_toggle, useAppSelector } from '../../../backend/reducers';
+import {
+    appDispatch,
+    app_close,
+    app_toggle,
+    useAppSelector
+} from '../../../backend/reducers';
 import { LazyComponent, ToolBar } from '../../../components/shared/general';
 import { externalLink } from '../../../backend/utils/constant';
 
@@ -117,25 +122,24 @@ export const FeedbackApp = () => {
             }
         }
 
-        // 	throw (i18next.t("error.NOT_RUNNING"));
         const { browser, os } = detectBrowserAndOS();
-        // await fetch(
-        //     'https://discord.com/api/webhooks/1158696317728600074/8QzsXoCsgY3oSTJvADvcRh3oeOel-Ofs6HK_aeJzD1nAMdwe2ppeI5bXO99rtVvfttOp',
-        //     {
-        //         method: 'POST',
-        //         headers: { 'Content-type': 'application/json; charset=UTF-8' },
-        //         body: JSON.stringify({
-        //             content: `
-		// 	Email: ${userName},
-		// 	Os: ${os}
-		// 	Browser: ${browser}
-		// 	Feeling: ${issues.feeling}
-		// 	Issue: ${issues.issue}
-		// 	Detail: ${issues.detail}
-		// 	`
-        //         })
-        //     }
-        // );
+        await fetch(
+            'https://discord.com/api/webhooks/1158696317728600074/8QzsXoCsgY3oSTJvADvcRh3oeOel-Ofs6HK_aeJzD1nAMdwe2ppeI5bXO99rtVvfttOp',
+            {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify({
+                    content: `
+			Email: ${user.email},
+			Os: ${os}
+			Browser: ${browser}
+			Feeling: ${issues.feeling}
+			Issue: ${issues.issue}
+			Detail: ${issues.detail}
+			`
+                })
+            }
+        );
         appDispatch(app_close('feedback'));
         appDispatch(app_toggle('payment'));
     };
@@ -149,7 +153,8 @@ export const FeedbackApp = () => {
         setSelector((prev) => ({ ...prev, text: value }));
     };
     return (
-        <div className="feedbackApp floatTab dpShad"
+        <div
+            className="feedbackApp floatTab dpShad"
             data-size={wnapp.size}
             id={wnapp.id + 'App'}
             data-max={wnapp.max}
@@ -170,84 +175,96 @@ export const FeedbackApp = () => {
                 data-dock="true"
             >
                 <LazyComponent show={!wnapp.hide}>
-
-                <h6 className="text-center mb-4 text-[14px] text-white">Feedback</h6>
-                <div className="flex p-2 items-center justify-between">
-                    {listFeedBack.map((icon) => (
-                        <div
-                            key={icon.value}
-                            onClick={() => {
-                                handleSelectFeeling(icon.value);
-                            }}
-                            className="flex flex-col items-center text-white"
-                        >
-                            <div className="flex rounded-[100%] border-gray-400 border-solid border bg-gray-200 w-max">
-                                <img
-                                    data-select={icon.value == selector.feeling}
-                                    src={icon.src}
-                                    className="icon"
-                                />
+                    <h6 className="text-center mb-4 text-[14px] text-white">
+                        Feedback
+                    </h6>
+                    <div className="flex p-2 items-center justify-between">
+                        {listFeedBack.map((icon) => (
+                            <div
+                                key={icon.value}
+                                onClick={() => {
+                                    handleSelectFeeling(icon.value);
+                                }}
+                                className="flex flex-col items-center text-white"
+                            >
+                                <div className="flex rounded-[100%] border-gray-400 border-solid border bg-gray-200 w-max">
+                                    <img
+                                        data-select={
+                                            icon.value == selector.feeling
+                                        }
+                                        src={icon.src}
+                                        className="icon"
+                                    />
+                                </div>
+                                <p className="text-[12px] pt-2">{icon.name}</p>
                             </div>
-                            <p className="text-[12px] pt-2">{icon.name}</p>
-                        </div>
-                    ))}
-                </div>
-                <hr className="my-[6px]" />
-                <p className="text-[14px] text-white">Bạn gặp vấn đề với:</p>
-                <div className="flex flex-wrap gap-1 justify-between mt-2">
-                    {listErr.map((err) => (
-                        <div
-                            key={err.value}
-                            style={isSelector(err.value)}
-                            onClick={() => {
-                                handleSelectErr(err.value);
+                        ))}
+                    </div>
+                    <hr className="my-[6px]" />
+                    <p className="text-[14px] text-white">
+                        Bạn gặp vấn đề với:
+                    </p>
+                    <div className="flex flex-wrap gap-1 justify-between mt-2">
+                        {listErr.map((err) => (
+                            <div
+                                key={err.value}
+                                style={isSelector(err.value)}
+                                onClick={() => {
+                                    handleSelectErr(err.value);
+                                }}
+                                className="p-1 text-center rounded-full border border-solid border-gray-700 ] text-[12px] text-white"
+                            >
+                                {err.name}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-wrap gap-1 my-2">
+                        {listErr.map((err) => {
+                            if (selector[err.value]?.choose == true) {
+                                return err.data.map((detail) => {
+                                    return (
+                                        <div
+                                            key={detail.value}
+                                            style={isSelector(detail.value)}
+                                            onClick={() => {
+                                                handleSelectErr(detail.value);
+                                            }}
+                                            className="p-1 m-auto text-center rounded-full border border-solid border-gray-700 ] text-[12px] text-white"
+                                        >
+                                            {detail.name}
+                                        </div>
+                                    );
+                                });
+                            }
+                        })}
+                    </div>
+                    <p className="text-[14px] mb-2 text-white">Chi Tiết:</p>
+                    <div className="border-solid border h-full rounded-md">
+                        <textarea
+                            className="noteText"
+                            value={selector.text}
+                            onInput={(e) => {
+                                handleInput(e.target.value);
                             }}
-                            className="p-1 text-center rounded-full border border-solid border-gray-700 ] text-[12px] text-white"
-                        >
-                            {err.name}
-                        </div>
-                    ))}
-                </div>
-                <div className="flex flex-wrap gap-1 my-2">
-                    {listErr.map((err) => {
-                        if (selector[err.value]?.choose == true) {
-                            return err.data.map((detail) => {
-                                return (
-                                    <div
-                                        key={detail.value}
-                                        style={isSelector(detail.value)}
-                                        onClick={() => {
-                                            handleSelectErr(detail.value);
-                                        }}
-                                        className="p-1 m-auto text-center rounded-full border border-solid border-gray-700 ] text-[12px] text-white"
-                                    >
-                                        {detail.name}
-                                    </div>
+                        ></textarea>
+                    </div>
+                    <button
+                        className="mt-4 mx-auto instbtn w-[120px] h-[40px]"
+                        onClick={() => {
+                            submitFeedback();
+                            if (
+                                selector.feeling != '' &&
+                                selector.feeling != 'good' &&
+                                selector.feeling != 'amazing'
+                            )
+                                window.open(
+                                    externalLink.FACEBOOK_MESSAGE_LINK,
+                                    '_blank'
                                 );
-                            });
-                        }
-                    })}
-                </div>
-                <p className="text-[14px] mb-2 text-white">Chi Tiết:</p>
-                <div className="border-solid border h-full rounded-md">
-                    <textarea
-                        className="noteText"
-                        value={selector.text}
-                        onInput={(e) => {
-                            handleInput(e.target.value);
                         }}
-                    ></textarea>
-                </div>
-                <button
-                    className="mt-4 mx-auto instbtn w-[120px] h-[40px]"
-                    onClick={() => {
-                        submitFeedback();
-                        if(selector.feeling != 'good' && selector.feeling != 'amazing')
-                            window.open(externalLink.FACEBOOK_MESSAGE_LINK, '_blank')
-                    }}
-                >
-                    Submit
-                </button>
+                    >
+                        Submit
+                    </button>
                 </LazyComponent>
             </div>
         </div>
