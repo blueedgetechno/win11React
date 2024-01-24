98e02aca58040186d6a04fa8ec1e7710e1418001
carosaome <sieunhankiet@gmail.com>
2024-01-24 17:30:08 +0700

    add modal worker detail

--- src/containers/popup/modal/worker_modal.jsx
+++ src/containers/popup/modal/worker_modal.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { combineText } from '../../../backend/utils/combineText';
 
-const ModalWorkerInfo = (info) => {
+export const worker_modal = (info) => {
     const renderDetailWorker = (data) => {
         const list = [];
         for (const key in data) {
@@ -8,27 +9,27 @@ const ModalWorkerInfo = (info) => {
                 break;
             }
             list.push(
-                <div key={Math.random()}>
-                    <span className="font-medium">
+                <React.Fragment key={Math.random()}>
+                    <span className="font-medium pl-5">
                         {data[key] && combineText(key)}
                     </span>
                     :<span> {typeof data[key] !== 'object' && data[key]}</span>
                     <div
                         style={{
-                            marginLeft: 15
+                            padding: 16,
                         }}
+                        className='bg-slate-500'
                     >
                         {typeof data[key] == 'object' &&
                             renderDetailWorker(data[key])}
                     </div>
-                </div>
+                </React.Fragment>
             );
         }
 
         return list;
     };
 
-    return <div className="p-5 h-[70vh]">{renderDetailWorker(info)}</div>;
+    return <div className="max-h-[70vh] bg-slate-500">{renderDetailWorker(info)}</div>;
 };
 
-export default ModalWorkerInfo;
