d0a174335528dd6c8f33b5916f55b3f8e00cf3b9
epitchi <thienvanlea1@gmail.com>
2024-01-24 19:12:41 +0700

    Revert "render storage name"
    This reverts commit 1beec4ba7bb8402086fdaa2f69546de1e9d5c72b.

--- src/containers/applications/apps/worker.jsx
+++ src/containers/applications/apps/worker.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     appDispatch,
     menu_hide,
@@ -6,9 +6,9 @@ import {
     worker_prev
 } from '../../../backend/reducers';
 import { combineText } from '../../../backend/utils/combineText';
-import { customClickDispatch } from '../../../backend/utils/dispatch';
 import { Icon, Image, ToolBar } from '../../../components/shared/general';
 import './assets/fileexpo.scss';
+import { customClickDispatch } from '../../../backend/utils/dispatch';
 
 export const Worker = () => {
     const wnapp = useAppSelector((state) =>
@@ -139,22 +139,6 @@ const ContentArea = ({ searchtxt, data }) => {
         return list;
     };
 
-    const renderName = (type, id) => {
-        let name
-        let workerFound = data.cdata.find((x) => id == x.id)?.info ?? {}
-        console.log(workerFound);
-        switch (type) {
-            case 'storage':
-                name = workerFound.owner
-                break;
-
-            default:
-                name = id
-                break;
-        }
-
-        return name
-    }
     return (
         <div
             className="contentarea"
@@ -185,7 +169,7 @@ const ContentArea = ({ searchtxt, data }) => {
                                             item.info
                                         )}`}
                                     />
-                                    <span>{renderName(item.type, item.id)}</span>
+                                    <span>{item.id}</span>
                                 </div>
                             )
                         );
