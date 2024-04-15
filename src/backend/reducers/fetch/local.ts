4db21d703e50deda16bf8aa0724e47134c02900e
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2024-04-15 21:19:13 +0700

    localtest

--- src/backend/reducers/fetch/local.ts
+++ src/backend/reducers/fetch/local.ts
@@ -12,7 +12,7 @@ async function internalFetch<T>(
     if (client != null) {
         if (command == 'info') {
             const { data, ok } = await client.get<T>(
-                `https://${address}:${WS_PORT}/info`,
+                `http://${address}:${WS_PORT}/info`,
                 {
                     timeout: { secs: 3, nanos: 0 },
                     responseType: ResponseType.JSON
@@ -24,7 +24,7 @@ async function internalFetch<T>(
             return data;
         } else {
             const { data, ok } = await client.post<T>(
-                `https://${address}:${WS_PORT}/${command}`,
+                `http://${address}:${WS_PORT}/${command}`,
                 Body.json(body),
                 {
                     responseType: ResponseType.JSON
@@ -37,7 +37,7 @@ async function internalFetch<T>(
         }
     } else {
         if (command == 'info') {
-            const resp = await fetch(`https://${address}/info`);
+            const resp = await fetch(`http://${address}/info`);
 
             if (!resp.ok) return new Error('fail to request');
 
@@ -184,11 +184,11 @@ export async function StartThinkmayOnVM(
         audioUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${resp.thinkmay.audioToken}&target=${target}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.audioToken}&target=${target}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.audioToken}&target=${target}`,
         videoUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${resp.thinkmay.videoToken}&target=${target}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.videoToken}&target=${target}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.videoToken}&target=${target}`,
         rtc_config: {
             iceTransportPolicy: 'relay',
             iceServers: [
@@ -242,11 +242,11 @@ export async function StartThinkmay(computer: Computer): Promise<Session> {
         audioUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${resp.thinkmay.audioToken}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.audioToken}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.audioToken}`,
         videoUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${resp.thinkmay.videoToken}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.videoToken}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${resp.thinkmay.videoToken}`,
         rtc_config: {
             iceTransportPolicy: 'all',
             iceServers: [
@@ -273,11 +273,11 @@ export function ParseRequest(
         audioUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${thinkmay.audioToken}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${thinkmay.audioToken}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${thinkmay.audioToken}`,
         videoUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${thinkmay.videoToken}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${thinkmay.videoToken}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${thinkmay.videoToken}`,
         rtc_config: {
             iceTransportPolicy: 'all',
             iceServers: [
@@ -305,11 +305,11 @@ export function ParseVMRequest(
         audioUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${thinkmay.audioToken}&target=${target}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${thinkmay.audioToken}&target=${target}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${thinkmay.audioToken}&target=${target}`,
         videoUrl:
             client == null
                 ? `https://${address}/handshake/client?token=${thinkmay.videoToken}&target=${target}`
-                : `https://${address}:${WS_PORT}/handshake/client?token=${thinkmay.videoToken}&target=${target}`,
+                : `http://${address}:${WS_PORT}/handshake/client?token=${thinkmay.videoToken}&target=${target}`,
         rtc_config: {
             iceTransportPolicy: 'relay', // preferred as VM often under double NAT
             iceServers: [
--- src/containers/applications/apps/connect.jsx
+++ src/containers/applications/apps/connect.jsx
@@ -1,116 +1,118 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useAppSelector } from '../../../backend/reducers';
 import {
-	useAppSelector
-} from '../../../backend/reducers';
-import { Icon, LazyComponent, ToolBar } from '../../../components/shared/general';
+    Icon,
+    LazyComponent,
+    ToolBar
+} from '../../../components/shared/general';
 
 import './assets/connect.scss';
 export const ConnectApp = () => {
-	const t = useAppSelector((state) => state.globals.translation);
-	const wnapp = useAppSelector((state) =>
-		state.apps.apps.find((x) => x.id == 'connectPc')
-	);
-	const [selector, setSelector] = useState({
-		feeling: '',
-		control: {
-			choose: false
-		},
-		text: ''
-	});
-	const user = useSelector((state) => state.user);
+    const t = useAppSelector((state) => state.globals.translation);
+    const wnapp = useAppSelector((state) =>
+        state.apps.apps.find((x) => x.id == 'connectPc')
+    );
+    const [selector, setSelector] = useState({
+        feeling: '',
+        control: {
+            choose: false
+        },
+        text: ''
+    });
+    const user = useSelector((state) => state.user);
 
-	const handleSelectFeeling = (feeling) => {
-		setSelector((prev) => ({ ...prev, feeling }));
-	};
-	const handleSelectErr = (err) => {
-		setSelector((prev) => {
-			return {
-				...prev,
-				[err]: { ...prev[err], choose: !prev[err]?.choose }
-			};
-		});
-	};
+    const handleSelectFeeling = (feeling) => {
+        setSelector((prev) => ({ ...prev, feeling }));
+    };
+    const handleSelectErr = (err) => {
+        setSelector((prev) => {
+            return {
+                ...prev,
+                [err]: { ...prev[err], choose: !prev[err]?.choose }
+            };
+        });
+    };
 
-	const isSelector = (key) => {
-		return {
-			opacity: selector[key]?.choose ? 1 : 0.5
-		};
-	};
+    const isSelector = (key) => {
+        return {
+            opacity: selector[key]?.choose ? 1 : 0.5
+        };
+    };
 
-	const handleInput = (value) => {
-		setSelector((prev) => ({ ...prev, text: value }));
-	};
+    const handleInput = (value) => {
+        setSelector((prev) => ({ ...prev, text: value }));
+    };
 
-	const listSpec = [
-		{
-			name: 'GPU:',
-			text: 'Nvidia RTX 3060Ti'
-		},
-		{
-			name: 'RAM:',
-			text: '12Gb Ram'
-		},
-		{
-			name: 'CPU:',
-			text: 'Intel Xeon™ (up to 3.1 GHz) 8 vCores'
-		},
-		{
-			name: 'STORAGE:',
-			text: '130GB'
-		},
-		{
-			name: 'OS:',
-			text: 'Window 10'
-		}
-	]
-	return (
-		<div
-			className="connectToPcApp floatTab dpShad"
-			data-size={wnapp.size}
-			id={wnapp.id + 'App'}
-			data-max={wnapp.max}
-			style={{
-				...(wnapp.size == 'cstm' ? wnapp.dim : null),
-				zIndex: wnapp.z
-			}}
-			data-hide={wnapp.hide}
-		>
-			<ToolBar
-				app={wnapp.id}
-				icon={wnapp.id}
-				size={wnapp.size}
-				name="Connect to your PC"
-			/>
-			<div
-				className="windowScreen connectAppContent flex flex-col p-[12px] pt-0"
-				data-dock="true"
-			>
-				<LazyComponent show={!wnapp.hide}>
-					<div className="content">
-						<div className='title'><Icon src='monitor'></Icon>Sieunhankiet's PC</div>
+    const listSpec = [
+        {
+            name: 'GPU:',
+            text: 'Nvidia RTX 3060Ti'
+        },
+        {
+            name: 'RAM:',
+            text: '12Gb Ram'
+        },
+        {
+            name: 'CPU:',
+            text: 'Intel Xeon™ (up to 3.1 GHz) 8 vCores'
+        },
+        {
+            name: 'STORAGE:',
+            text: '130GB'
+        },
+        {
+            name: 'OS:',
+            text: 'Window 10'
+        }
+    ];
+    return (
+        <div
+            className="connectToPcApp floatTab dpShad"
+            data-size={wnapp.size}
+            id={wnapp.id + 'App'}
+            data-max={wnapp.max}
+            style={{
+                ...(wnapp.size == 'cstm' ? wnapp.dim : null),
+                zIndex: wnapp.z
+            }}
+            data-hide={wnapp.hide}
+        >
+            <ToolBar
+                app={wnapp.id}
+                icon={wnapp.id}
+                size={wnapp.size}
+                name="Connect to your PC"
+            />
+            <div
+                className="windowScreen connectAppContent flex flex-col p-[12px] pt-0"
+                data-dock="true"
+            >
+                <LazyComponent show={!wnapp.hide}>
+                    <div className="content">
+                        <div className="title">
+                            <Icon src="monitor"></Icon>Sieunhankiet's PC
+                        </div>
 
-						<div className=' flex-1 mt-10 display flex flex-col justify-between'>
-							<div className="flex flex-col gap-3">
+                        <div className=" flex-1 mt-10 display flex flex-col justify-between">
+                            <div className="flex flex-col gap-3">
+                                {listSpec.map((spec) => (
+                                    <div key={spec.text} className="spec">
+                                        <b className="min-w-[100px]">
+                                            {spec.name}
+                                        </b>
+                                        {spec.text}
+                                    </div>
+                                ))}
+                            </div>
 
-								{listSpec.map(spec => (
-									<div key={spec.text} className='spec'>
-										<b className='min-w-[100px]'>{spec.name}</b>
-										{spec.text}
-									</div>
-								))}
-
-
-							</div>
-
-							<button className='instbtn connectBtn'>
-								Connect
-							</button>
-						</div>
-
-					</div>
-				</LazyComponent>
-			</div>
-		</div>
-	);
+                            <button className="instbtn connectBtn">
+                                Connect
+                            </button>
+                        </div>
+                    </div>
+                </LazyComponent>
+            </div>
+        </div>
+    );
 };
