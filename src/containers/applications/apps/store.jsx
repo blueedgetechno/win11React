4612291c8204b96106d7d9e00ed85cb1654569a3
win11bot <w11bot@andrewstech.me>
2024-05-05 10:38:37 +0000

    Prettier
    skip actions

--- src/containers/applications/apps/store.jsx
+++ src/containers/applications/apps/store.jsx
@@ -115,7 +115,9 @@ export const MicroStore = () => {
   useEffect(() => {
     if (!wnapp.hide && fetchState == 0) {
       var url = queryParams.get("customstore");
-      if (!url) url = "https://raw.githubusercontent.com/win11react/store/main/store/index.json";
+      if (!url)
+        url =
+          "https://raw.githubusercontent.com/win11react/store/main/store/index.json";
 
       axios
         .get(url)
